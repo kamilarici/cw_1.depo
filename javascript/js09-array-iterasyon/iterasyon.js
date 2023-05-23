daa0a70dab4afe97f1a70ba01b2a973766929971
KamilArici <kamilarici5@gmail.com>
2023-05-23 13:21:40 +0300

    map

---
+++ javascript/js09-array-iterasyon/iterasyon.js
@@ -0,0 +1,23 @@
+const prices = [250, 150, 300, 500];
+
+// prices.forEach((x, i) => console.log("değer:", x, "indis", i));
+
+let toplam = 0;
+prices.forEach((x) => console.log(x * 2));
+
+let names = ["mustafa", "murat", "ahmet", "mustafa", "ayşe", "canan"];
+
+names = names.map((name) => name.toLocaleUpperCase());
+
+// console.log(upperName);
+console.log(names);
+
+// const euro = 21.78;
+// const dolar = 19.8;
+// const tlPrices = [100, 150, 50, 80];
+
+// const dolarPrices = tlPrices.map((tl) => +(tl / dolar).toFixed(2));
+// const euroPrices = tlPrices.map((tl) => +(tl / euro).toFixed(2));
+// console.log(dolarPrices);
+// console.log(euroPrices);
+// console.log(tlPrices);
---
+++ javascript/js09-array-iterasyon/loops.js
@@ -0,0 +1,52 @@
+//************** */
+// const num1 = [1, 5, 8, 9, 14, 55, 78, 63];
+
+// let toplam = 0;
+// for (let i = 0; i < num1.length; i++) {
+//   toplam += num1[i];
+// }
+// console.log(toplam);
+// console.log((toplam / num1.length).toFixed());
+// console.log(29 * 8);
+
+// const num1 = [1, 5, 8, 9, 14, 55, 78, 63];
+// let küçük15 = [];
+// let büyük15 = [];
+// for (let i = 0; i < num1.length; i++) {
+//   if (num1[i] < 15) {
+//     küçük15.push(num1[i]);
+//   } else {
+//     büyük15.push(num1[i]);
+//   }
+// }
+// console.log(küçük15);
+// console.log(büyük15);
+
+// const num1 = [1, 5, 8, 9, 14, 55, 78, 63];
+// let küçük15 = [];
+// let büyük15 = [];
+// for (let i in num1) {
+//   num1[i] < 15 ? küçük15.push(num1[i]) : büyük15.push(num1[i]);
+// }
+
+// console.log(küçük15);
+// console.log(büyük15);
+
+//? -****************
+/* const sut1 = ["mehmet", "Ahmet", "ismet", "Saffet", "kamil", "Ahmet"];
+
+const findsut1 = (name) => {
+  let sayac = 0;
+
+  for (let sud of sut1) {
+    sud === name.toLowerCase() && sayac++;
+  }
+
+  return sayac === 0
+    ? `${name} bulunamamıştır`
+    : `${name}den ${sayac} adet bulunmuştur`;
+};
+
+console.log(findsut1("AhmeT"));
+console.log(findsut1("kAmil"));
+console.log(findsut1("ahmeT")); */
