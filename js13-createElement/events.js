9d3fbff5bf14d40d35db7fdc946319c2223e5d6b
KamilArici <kamilarici5@gmail.com>
2023-06-04 23:37:37 +0300

    events

--- js13-createElement/events.js
+++ js13-createElement/events.js
@@ -1,1 +1,25 @@
 console.log("events");
+
+const h1 = document.querySelector("header h1");
+
+h1.onmouseover = function () {
+  h1.style.color = "aqua";
+};
+h1.onmouseout = function () {
+  h1.style.color = "blue";
+};
+
+const phello = () => {
+  document.write("hello");
+};
+
+phello();
+
+window.addEventListener("load", () => {
+  document.getElementById("input").focus();
+});
+
+const addbutton = document.querySelector("#btn");
+addbutton.addEventListener("click", () => {
+  console.log("btn click");
+});
