bf8b7136791da8d2b22abf15a183befd4e927333
KamilArici <kamilarici5@gmail.com>
2023-06-05 03:36:24 +0300

    ödev delete

--- js13-createElement/events.js
+++ js13-createElement/events.js
@@ -39,7 +39,7 @@ addBtn.addEventListener("click", (e) => {
   //   console.log(e)
   //   console.log(e.target)
   //   console.log(e.target.value)
-
+  //   console.log(object);
   if (!input.value) {
     alert("Please enter a comment");
   } else {
@@ -48,6 +48,7 @@ addBtn.addEventListener("click", (e) => {
     ul.appendChild(li);
     const textLi = document.createTextNode(input.value);
     li.appendChild(textLi);
+
     input.value = "";
     input.focus();
   }
@@ -55,10 +56,31 @@ addBtn.addEventListener("click", (e) => {
 
 //? input'un üzerinde iken bir klavye tusuna basilirsa calis
 document.querySelector("#input").addEventListener("keydown", (e) => {
-  //   console.log(e.code)
-  //   console.log(e.keyCode)
+  //   console.log(e.code);
+  //   console.log(e.keyCode);
   //? Basilan tus Enter ise
   if (e.code === "Enter") {
     addBtn.click(); //? add butonun tiklanma fonksiyonu calistir.
   }
 });
+
+//?   ****** Ödev  ********
+//! ****************************
+const delbutton = document.querySelector("#delete");
+
+delbutton.addEventListener("click", (e) => {
+  const li = ul.querySelectorAll("ul li");
+  if (li.length === 0) {
+    alert("silinecek birşey kalmadı");
+  } else {
+    li[li.length - 1].remove();
+  }
+});
+
+document.querySelector("#input").addEventListener("keydown", (e) => {
+  //? Basilan tus delete ise
+  if (e.code === "Delete") {
+    delbutton.click(); //? add butonun tiklanma fonksiyonu calistir.
+  }
+});
+// ! **********************
