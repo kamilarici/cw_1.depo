7765e8a57d66c2330fe1d0939f3d94d42f0488ee
KamilArici <kamilarici5@gmail.com>
2023-06-06 22:21:36 +0300

    selection article

--- js14-dom-taş-kağıt-makas/app.js
+++ js14-dom-taş-kağıt-makas/app.js
@@ -1,18 +1,46 @@
 //! *************selector
-const rockImg = document.getElementById("rock");
-const paperImg = document.getElementById("paper");
-const scissorImg = document.getElementById("scissor");
+const selectionArticle = document.querySelector(".selection");
 
 //? seçilen elemenların taşıyıcıları******
 const yourChoiceDiv = document.getElementById("your-choice");
 
 //! *******veriables
-let image = document.createElement("img");
+let userSelectImg = document.createElement("img");
 //! **************event listeners
-rockImg.addEventListener("click", () => {
-  image.src = src = "./assets/rock.png";
-  image.alt = "rock";
-  yourChoiceDiv.appendChild(image);
-  //? ****inner html
-  //   yourChoiceDiv.innerHTML = `<img src="./assets/rock.png"> alt="rock"`;
+
+selectionArticle.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  if (e.target.id) {
+  }
+
+  userSelectImg.src = src = `./assets/${e.target.id}.png`;
+  userSelectImg.alt = `${e.target.id}`;
+  yourChoiceDiv.appendChild(userSelectImg);
 });
+
+//? *****resimler yakala
+// const rockImg = document.getElementById("rock");
+// const paperImg = document.getElementById("paper");
+// const scissorImg = document.getElementById("scissor");
+
+// rockImg.addEventListener("click", () => {
+//   image.src = src = "./assets/rock.png";
+//   image.alt = "rock";
+//   yourChoiceDiv.appendChild(image);
+//   //? ****inner html
+//   //   yourChoiceDiv.innerHTML = `<img src="./assets/rock.png"> alt="rock"`;
+// });
+// paperImg.addEventListener("click", () => {
+//   image.src = src = "./assets/paper.png";
+//   image.alt = "paper";
+//   yourChoiceDiv.appendChild(image);
+//   //? ****inner html
+//   //   yourChoiceDiv.innerHTML = `<img src="./assets/rock.png"> alt="rock"`;
+// });
+// scissorImg.addEventListener("click", () => {
+//   image.src = src = "./assets/scissor.png";
+//   image.alt = "scissor";
+//   yourChoiceDiv.appendChild(image);
+//? ****inner html
+//   yourChoiceDiv.innerHTML = `<img src="./assets/rock.png"> alt="rock"`;
+// });
