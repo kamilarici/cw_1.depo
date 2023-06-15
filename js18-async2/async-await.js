c08401db54cd30858669f253f9454ce0638a159e
KamilArici <kamilarici5@gmail.com>
2023-06-15 23:07:54 +0300

    SON

--- js18-async2/async-await.js
+++ js18-async2/async-await.js
@@ -18,3 +18,60 @@
 //* satirdaki kodun durdurulmasini saglar.
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
+const getNews = async () => {
+  const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78e";
+
+  const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+  try {
+    const res = await fetch(URL);
+    console.log(res);
+    //?Error handling
+    if (!res.ok) {
+      throw new Error("News can not be fetched");
+    }
+    const data = await res.json();
+    renderNews(data.articles);
+  } catch (err) {
+    // console.log(error)
+    renderError(err);
+  }
+};
+
+const renderError = (err) => {
+  const newsDiv = document.getElementById("news");
+  newsDiv.innerHTML = `
+      <h3>${err}</h3>
+      <img src="./img/404.png" alt="404" />
+    `;
+};
+
+const renderNews = (news) => {
+  console.log(news);
+  const newsDiv = document.getElementById("news");
+
+  news.map((item) => {
+    const { title, description, content, url, urlToImage } = item; //? destructure
+    newsDiv.innerHTML += `
+      <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="card">
+              <img src="${urlToImage}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text">${content}</p>
+                  <a href="${url}" target="_blank" class="btn btn-danger">Go Detail</a>
+              </div>
+          </div>
+      </div>
+      
+      `;
+  });
+};
+
+window.addEventListener("load", () => {
+  getNews();
+});
+
+// https://restcountries.com/v3.1/all
+// https://flags-cw.netlify.app/
+// https://newsapi.org/v2/top-headlines?country=us&apiKey=47d350b7a633401694e0fa5b4ddcfd46
