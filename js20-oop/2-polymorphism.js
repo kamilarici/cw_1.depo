69416a8eaf02f10af4e72c1cc075564d5068936e
KamilArici <kamilarici5@gmail.com>
2023-06-20 21:45:59 +0300

    polimorphism

--- js20-oop/2-polymorphism.js
+++ js20-oop/2-polymorphism.js
@@ -7,4 +7,37 @@
 //* Polymorphism, genellikle Overloading ve Overriding gibi alt kavramlar
 //* ile bilinir.
 
-console.log("Polymorphism")
+console.log("polymorphisim");
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummary() {
+    return `${this.title} waswritten by ${this.author} in ${this.year}`;
+  }
+}
+//! Book kalibinda yeni bir ornek (instance) olusturduk.
+const book3 = new Book("stupid reseachers", "xyz", 2022);
+const book4 = new Book("dummy reseachers", "abc", 2022);
+const book5 = new Book("yeni ad", "mmö", 2020);
+
+console.log(book3);
+
+console.log(book3.getSummary());
+console.log(book4.getSummary());
+
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+
+const mag1 = new Magazine("elle", "allen mcalle", 2023, "june");
+
+console.log(mag1);
+console.log(mag1.getSummary());
