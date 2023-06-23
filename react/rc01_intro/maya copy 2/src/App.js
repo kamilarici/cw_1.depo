16be10d6128a0a15af089bc96f61ecf86566df5e
KamilArici <kamilarici5@gmail.com>
2023-06-23 18:53:02 +0300

    maya copy

--- react/rc01_intro/maya copy 2/src/App.js
+++ react/rc01_intro/maya copy 2/src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1>Helllllo React</h1>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
