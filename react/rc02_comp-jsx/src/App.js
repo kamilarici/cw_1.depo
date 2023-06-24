81e3e8e9c7672a1be4eb8a0c6cbb72aedead2e84
KamilArici <kamilarici5@gmail.com>
2023-06-24 12:17:08 +0300

    export-inport

--- react/rc02_comp-jsx/src/App.js
+++ react/rc02_comp-jsx/src/App.js
@@ -1,9 +1,12 @@
+import { Header } from "./Header";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react/rc02_comp-jsx/src/Header.js
@@ -0,0 +1,13 @@
+export function Header() {
+  //? js
+  return (
+    <>
+      {/*js*/}
+      <h1>Header</h1>
+      <p>cw</p>
+      {}
+    </>
+  );
+}
+
+// export default Header;
