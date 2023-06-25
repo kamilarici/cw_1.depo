604ddbbd27bdbdc4e3fdaee6d5e05e461528743a
KamilArici <kamilarici5@gmail.com>
2023-06-26 01:23:43 +0300

    css

--- react-proje/react-proje-1/src/Header.jsx
+++ react-proje/react-proje-1/src/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="header1">
       <div className="header1-img">
-        <img src={img} alt="" />
+        <h1>TREND-CAR</h1>
       </div>
       <div className="header1-home">
         <a href="#">Home</a>
