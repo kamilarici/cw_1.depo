e9583d1bcdf2ef3252bf7e4d70e7a1497af866ff
KamilArici <kamilarici5@gmail.com>
2023-06-26 15:33:43 +0300

    boostrap

--- react-calısma/src/conponents/User.jsx
+++ react-calısma/src/conponents/User.jsx
@@ -4,12 +4,20 @@ import PropTypes from "prop-types";
 const User = (props) => {
   const { name, department, salary } = props;
   return (
-    <div>
-      <ul>
-        <li>isim:{name} </li>
-        <li>departman:{department}</li>
-        <li>maas: {salary}</li>
-      </ul>
+    <div className="col-md-8 mb-4">
+      <div className="card">
+        <div className="card-header d-flex justify-content-between">
+          <h4 className="d-inline">{name}</h4>
+          <i
+            className="fa-sharp fa-solid fa-trash"
+            style={{ cursor: "pointer" }}
+          ></i>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Maas:{salary}</p>
+          <p className="card-text">department:{department}</p>
+        </div>
+      </div>
     </div>
   );
 };
