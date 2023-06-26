4964082ffd031341fc81755d565e9f62e4ece066
KamilArici <kamilarici5@gmail.com>
2023-06-26 14:08:58 +0300

    bootsrap

---
+++ react-calısma/src/conponents/User.jsx
@@ -0,0 +1,12 @@
+const User = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <button>Gönder</button>
+      </form>
+    </div>
+  );
+};
+
+export default User;
