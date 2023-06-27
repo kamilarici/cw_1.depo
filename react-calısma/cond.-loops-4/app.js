950abb4606defe4c3929f2848dce4fdaa49ca18a
KamilArici <kamilarici5@gmail.com>
2023-06-27 16:00:54 +0300

    while

---
+++ react-calısma/cond.-loops-4/app.js
@@ -0,0 +1,30 @@
+console.log("cond.");
+
+const yasSiniri = 20;
+const ahmetinYasi = 15;
+const mehmetinYasi = 25;
+const kmlinYasi = 33;
+
+if (ahmetinYasi > yasSiniri) {
+  console.log("buyukmus");
+} else {
+  console.log("kucukmus");
+}
+
+const bool1 = true;
+const bool2 = true;
+const bool3 = false;
+
+if (bool3 == true && bool2 == true) {
+  console.log("truymus");
+} else {
+  console.log("true degil");
+}
+
+//?while döngüsü
+let number = 5;
+while (number < 20) {
+  console.log(number);
+  number += 1;
+}
+console.log(number);
