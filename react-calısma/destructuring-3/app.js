bc64126e682a4ba45a081fe2254035a2f6844c43
KamilArici <kamilarici5@gmail.com>
2023-06-27 13:11:47 +0300

    array-destr.

--- react-calısma/destructuring-3/app.js
+++ react-calısma/destructuring-3/app.js
@@ -5,6 +5,10 @@ const user = {
   age: 25,
   isTest: false,
   skills: ["html", "css"],
+  school: {
+    name: "asd",
+    sinif: 3,
+  },
 };
 //*uzun yol
 // const name = user.name;
@@ -16,5 +20,21 @@ const user = {
 // const { name, age, isTest, skills } = user;
 // console.log(name, age, isTest, skills);
 //? buradaki rest değişken herhangi birşey olabilir .ahmet mehmet herşey olabilir .önündeki üç nokta  spread operatörüdür ve arrayin içindekileri dışarı çıkarır ekler.
-const { name, age, ...rest } = user;
-console.log(rest);
+// const { name, age, ...rest } = user;
+// console.log(rest); //{isTest: false, skills: Array(2)}
+
+// const { school } = user;
+// console.log(school);//{name: 'asd', sinif: 3}
+
+//! iç içe seviyede destr.yöntemi ..içine girildiğinde objenin kendini çağıramazsınız sadece girdiğiniz yerdeki değeri çağırabilirsiniz
+// const {
+//   school: { sinif, name },
+// } = user;
+// console.log(sinif, name); //3 'asd'
+
+//? ARRAY DESTR.
+const skills = ["html", "css", "js"];
+
+const firstSkills = skills[0];
+const secondSkills = skills[1];
+console.log(firstSkills, secondSkills); //html css
