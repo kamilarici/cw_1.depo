62ce7cccd4cd669bdb4e8bbbc2d0c97bb7110ecb
KamilArici <kamilarici5@gmail.com>
2023-06-28 12:52:28 +0300

    callbackhell

--- react-calısma/async/app.js
+++ react-calısma/async/app.js
@@ -1,1 +1,30 @@
 console.log("async");
+
+function getUser(callback) {
+  console.log("getuser içerisinde");
+  setTimeout(() => {
+    console.log("settimeout içerisi");
+    callback({ username: "mehmet" });
+  }, 2000);
+}
+
+function getAdresses(username, callback) {
+  setTimeout(() => {
+    callback(["ankara", "mugla", "eskisehir"]);
+  }, 1000);
+}
+
+function getRestaruants(addresses, callback) {
+  setTimeout(() => {
+    callback(["a restoran", "b restoran", "c restoran"]);
+  }, 1000);
+}
+getUser((user) => {
+  console.log(user);
+  getAdresses(user.username, (addresses) => {
+    getRestaruants(addresses, (openRestaurants) => {
+      document.body.append("acıik restoranlar:", openRestaurants.toString());
+    });
+  });
+});
+console.log("son console");
