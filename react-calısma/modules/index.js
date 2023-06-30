737b422180408aedc9685c670a8285be499e9339
KamilArici <kamilarici5@gmail.com>
2023-06-30 23:40:39 +0300

    module import

---
+++ react-calısma/modules/index.js
@@ -0,0 +1,12 @@
+console.log("merhaba");
+
+import { CALISMA_SAATI, maas } from "./mymodule.js";
+import { arttir as inc, azalt as dec } from "./mymodule.js";
+import selam from "./mymodule.js";
+console.log(CALISMA_SAATI);
+console.log(maas);
+
+console.log(inc(5));
+console.log(dec(5));
+
+selam();
