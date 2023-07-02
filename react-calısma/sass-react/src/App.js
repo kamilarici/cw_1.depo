366b5e73e6f8f28141e7cf0d7004bfa59028b608
KamilArici <kamilarici5@gmail.com>
2023-07-02 14:47:17 +0300

    sass

--- react-calısma/sass-react/src/App.js
+++ react-calısma/sass-react/src/App.js
@@ -1,9 +1,11 @@
+import "./style/menu.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1 className="kutu">Hello React</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
