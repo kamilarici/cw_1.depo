64026865d3f58c176bf52811b0fc99958d5da569
KamilArici <kamilarici5@gmail.com>
2023-07-02 11:44:08 +0300

    scc

---
+++ react-calısma/css/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p style={{ color: "red", backgroundColor: "beige", padding: 30 }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+        ducimus praesentium quibusdam accusamus sunt, maiores molestias iusto
+        alias in rerum.
+      </p>
+    </div>
+  );
+}
+
+export default App;
