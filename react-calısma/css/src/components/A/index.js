9fd03262e847f83003264fa13cb9822b2c6b9683
KamilArici <kamilarici5@gmail.com>
2023-07-02 12:38:55 +0300

    class-id -syle

--- react-calısma/css/src/components/A/index.js
+++ react-calısma/css/src/components/A/index.js
@@ -4,6 +4,9 @@ const A = () => {
   return (
     <div className={style.title}>
       <header className={style.header}>react js</header>
+      <p className={style.p}>Lorem ipsum dolor sit amet.</p>
+      <h5 className={style.h5}>kkkk</h5>
+      <button id={style.click}>aclick</button>
     </div>
   );
 };
--- react-calısma/css/src/components/B/index.js
+++ react-calısma/css/src/components/B/index.js
@@ -4,6 +4,9 @@ const B = () => {
   return (
     <div className={style.title}>
       <header className={style.header}>kml arc</header>
+      <p className={style.p}>Lorem ipsum dolor sit amet.</p>
+      <h5 className={style.h5}>kkkkb</h5>
+      <button id={style.click}>click</button>
     </div>
   );
 };
