1bd9e78676c2a6e71876b512981b59d0c2fafbd0
KamilArici <kamilarici5@gmail.com>
2023-07-03 20:37:55 +0300

    card-button

--- react/rc03_module-css/src/App.js
+++ react/rc03_module-css/src/App.js
@@ -1,9 +1,13 @@
+import Card from "./components/card/Card";
+import data from "./data";
+console.log(data);
 function App() {
   return (
     <>
       <h1>APP</h1>
+      <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react/rc03_module-css/src/components/card/Card.jsx
@@ -0,0 +1,13 @@
+import Button from "../button/Button";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div>
+      <h1>card</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default Card;
