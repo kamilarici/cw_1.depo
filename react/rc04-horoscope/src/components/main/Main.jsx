496a243cad4d374d99d973d7f3e493acf0c4c468
KamilArici <kamilarici5@gmail.com>
2023-07-05 10:16:30 +0300

    spread

--- react/rc04-horoscope/src/components/main/Main.jsx
+++ react/rc04-horoscope/src/components/main/Main.jsx
@@ -7,6 +7,7 @@ const Main = () => {
     <div className="card-container">
       {data.map((item) => {
         //?key göndermemiz gerekiyor çünkü bunu react yazmamızı istiyor.dom da yazmasa bile yazmamız gerekiyor
+        //?spread operatörü ile açarak da yollayabiliriz yolladığımız yerde clg yaparak gelenlerin açıldığını görebiliriz
         return <Card key={item.id} {...item} />;
       })}
     </div>
