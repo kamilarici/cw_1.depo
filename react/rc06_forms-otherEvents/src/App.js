1b85378777cef1652d6369701d899e263f18d692
KamilArici <kamilarici5@gmail.com>
2023-07-06 20:51:46 +0300

    onChange-value

--- react/rc06_forms-otherEvents/src/App.js
+++ react/rc06_forms-otherEvents/src/App.js
@@ -1,7 +1,7 @@
 import Form from "./components/1-forms/Form";
 import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
 import MouseEvent from "./components/3-mouseEvents/MouseEvent";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div>
--- react/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ react/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,5 +1,58 @@
-const Form = () => {
-  return <div>Form</div>
-}
+import { useState } from "react";
 
-export default Form
+const Form = () => {
+  const [username, setUsername] = useState("");
+
+  const handleUsername = (e) => {
+    // console.log(e.target.value)
+    setUsername(e.target.value);
+  };
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center text-success">FORMS IN REACT</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Hello {username}
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="emailHelp"
+            //? OnChange event'ı input degeri her degistiginde tetiklenir. Biz de yazdıgımız event handler araciligi ile State'i guncelleyebilmis oluruz.
+            onChange={handleUsername}
+            value={username}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+
+        <div className="text-center">
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
