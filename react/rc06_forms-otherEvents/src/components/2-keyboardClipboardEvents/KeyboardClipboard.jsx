8a4fe53679ba4a604481de628eca99beb1e5f43e
KamilArici <kamilarici5@gmail.com>
2023-07-06 22:16:17 +0300

    onkeydawn

--- react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ react/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -2,20 +2,24 @@ import { useState } from "react";
 
 const KeyboardClipboard = () => {
   const [content, setContent] = useState("");
+
   return (
     <div className="container mt-4">
-      <h2 className="text-center">keybord clipboard</h2>
+      <h2 className="text-center">Keyboard-Clipboard</h2>
 
       <input
         type="text"
         className="form-control"
         onChange={(e) => {
-          e.target.value = e.target.value.toLocaleUpperCase();
-          setContent(e.target.value);
+          // e.target.value = e.target.value.toLocaleUpperCase()
+          setContent(e.target.value.toLocaleUpperCase());
         }}
+        onKeyDown={(e) => console.log(e.keyCode)}
+        value={content}
       />
+
       <div className="mt-4">
-        <h3>copied content</h3>
+        <h3>Copied Content</h3>
         <p>{content.toLocaleLowerCase()}</p>
       </div>
     </div>
