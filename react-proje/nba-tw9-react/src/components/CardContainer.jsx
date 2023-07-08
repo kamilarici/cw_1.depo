1478cb5123d6aef39492e5c16c5a9e124b0f3ed1
KamilArici <kamilarici5@gmail.com>
2023-07-08 18:57:50 +0300

    reflesh

--- react-proje/nba-tw9-react/src/components/CardContainer.jsx
+++ react-proje/nba-tw9-react/src/components/CardContainer.jsx
@@ -15,10 +15,10 @@ const CardContainer = (props) => {
       <div className="relative" onClick={handleClick}>
         <img className="card-img" src={img} alt="" />
         <ul className={` ul ${active ? "ulz" : ""}`}>
-          <li>🌟{statistics[0]}</li>
-          <li>🌟{statistics[1]}</li>
-          <li>🌟{statistics[2]}</li>
-          <li>🌟{statistics[3]}</li>
+          <li>🌟🏀{statistics[0]}</li>
+          <li>🌟🏀{statistics[1]}</li>
+          <li>🌟🏀{statistics[2]}</li>
+          <li>🌟🏀{statistics[3]}</li>
         </ul>
       </div>
       <p className=" p-card">{name}</p>
