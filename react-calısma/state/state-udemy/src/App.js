4585bafa8b2104b4685ab6baf0cedad72adfb1d7
KamilArici <kamilarici5@gmail.com>
2023-07-09 13:27:04 +0300

    end

--- react-calısma/state/state-udemy/src/App.js
+++ react-calısma/state/state-udemy/src/App.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import Course from "./Course";
+
+function getRondomCourse() {
+  const courseArray = ["batman", "dr", "thor", "black"];
+  return courseArray[Math.floor(Math.random() * 4)];
+}
 
 function App() {
-  const [value, setValue] = useState(0);
-
+  const [course, setCourse] = useState([]);
+  const handleClick = () => {
+    setCourse([...course, getRondomCourse()]);
+  };
   return (
     <div className="App">
       <button onClick={handleClick}>kurs ekle</button>
-      <div>kurs sayısı:{value}</div>
+      <div className="flex">
+        {course.map((item, index) => {
+          return <Course key={index} courseName={item} />;
+        })}
+      </div>
     </div>
   );
 }
