dc6a929424ce69f1c3cc2b54c976258e99b92dd6
KamilArici <kamilarici5@gmail.com>
2023-07-09 17:01:18 +0300

    bmı start

---
+++ react-proje/bmı-hook/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>BMI Calculater</h2>
+        <form action="">
+          <div>
+            <label htmlFor="">weight</label>
+            <input type="text" playcolder="enter weight value" value={weight} onChange={} />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
