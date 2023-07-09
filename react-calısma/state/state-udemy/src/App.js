1b954427b23141bb0c4c4cf202c4150119951bf2
KamilArici <kamilarici5@gmail.com>
2023-07-09 11:42:04 +0300

    onclick

--- react-calısma/state/state-udemy/src/App.js
+++ react-calısma/state/state-udemy/src/App.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      <h1>usestate</h1>
-      <h2>bmi project</h2>
+      <button onClick={handleClick}>kurs ekle</button>
+      <div>kurs sayısı:{value}</div>
     </div>
   );
 }
