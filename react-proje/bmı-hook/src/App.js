d54f5162d3369c02a6751607184e68260c5becc7
KamilArici <kamilarici5@gmail.com>
2023-07-09 19:00:47 +0300

    end bmi

--- react-proje/bmı-hook/src/App.js
+++ react-proje/bmı-hook/src/App.js
@@ -1,12 +1,86 @@
+import { useState } from "react";
+import "./app.css";
+import healty from "./assets/healty.png";
+import over from "./assets/overweight.png";
+import under from "./assets/underweight.png";
+
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  let calcBmi = (e) => {
+    e.preventDefault();
+    if (weight === 0 || height === 0) {
+      alert("please enter a valid weight and height");
+    } else {
+      let bmi = (weight / (height * height)) * 703;
+      setBmi(bmi.toFixed(1));
+      console.log(bmi);
+
+      if (bmi < 25) {
+        setMessage("you are underweight");
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage("you are a healthy weight");
+      } else {
+        setMessage("you are overweiht");
+      }
+    }
+  };
+
+  let imgSrc;
+  if (bmi < 1) {
+    imgSrc = null;
+  } else {
+    if (bmi < 25) {
+      imgSrc = under;
+    } else if (bmi >= 25 && bmi < 30) {
+      imgSrc = healty;
+    } else {
+      imgSrc = over;
+    }
+  }
+  let reload = () => {
+    window.location.reload();
+  };
   return (
     <div className="App">
       <div className="container">
-        <h2>BMI Calculater</h2>
-        <form action="">
+        <h2 className="center">BMI Calculater</h2>
+        <form action="" onSubmit={calcBmi}>
           <div>
             <label htmlFor="">weight</label>
-            <input type="text" playcolder="enter weight value" value={weight} onChange={} />
+            <input
+              type="text"
+              playcolder="enter weight value"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="">height</label>
+            <input
+              type="text"
+              playcolder="enter height value"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="btnn " type="submit">
+              submit
+            </button>
+            <button className="btnn btn-outline" type="submit" onClick={reload}>
+              reload
+            </button>
+          </div>
+          <div className="center">
+            <h3> your bmı is: {bmi}</h3>
+            <p>{message}</p>
+          </div>
+          <div className="img-container">
+            <img src={imgSrc} alt="" />
           </div>
         </form>
       </div>
