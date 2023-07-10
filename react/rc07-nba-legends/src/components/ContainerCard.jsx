55f13504c502c2f98175f223ba5de5f6ff4fcf3c
KamilArici <kamilarici5@gmail.com>
2023-07-10 21:53:53 +0300

    search

--- react/rc07-nba-legends/src/components/ContainerCard.jsx
+++ react/rc07-nba-legends/src/components/ContainerCard.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const ContainerCard = () => {
+  const [search, setSearch] = useState("");
+  const handleChange = (e) => {
+    console.log(e.target.value); //!asenkron olarak çalışır
+    setSearch(e.target.value);
+  };
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." />
-      <Container className="p-3 rounded-4 card-container">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
+      <Container className="p-3 rounded-4 card-container my-3">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3 ">
           <Col>1 of 2</Col>
           <Col>2 of 2</Col>
           <Col>3 of 2</Col>
