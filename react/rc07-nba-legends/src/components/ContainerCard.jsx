1774a1585fd01efdf50dbb671ae6a4107edc77d1
KamilArici <kamilarici5@gmail.com>
2023-07-10 22:50:08 +0300

    player card

--- react/rc07-nba-legends/src/components/ContainerCard.jsx
+++ react/rc07-nba-legends/src/components/ContainerCard.jsx
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
 
 const ContainerCard = () => {
   const [search, setSearch] = useState("");
   console.log(data);
   const handleChange = (e) => {
-    console.log(e.target.value); //!asenkron olarak çalışır
-    setSearch(e.target.value);
+    console.log(e.target.value);
+    setSearch(e.target.value); //! setter mtodları asenkron olarak çalışır.
+    // console.log(search)
   };
+  console.log(search);
+
   const filteredData = data.filter((item) =>
     item.name.toLowerCase().includes(search.trim().toLowerCase())
   );
-  console.log(filteredData);
+
+  console.log("filter", filteredData);
+
   return (
     <>
       <Form.Control
@@ -24,10 +30,20 @@ const ContainerCard = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3 ">
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>3 of 2</Col>
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
+          {/* {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              player={player}
+              img={player.img}
+              name={player.name}
+              statistics={player.statistics}
+              {...player}
+            />
+          ))} */}
+          {filteredData.map((player, i) => (
+            <PlayerCard key={i} {...player} />
+          ))}
         </Row>
       </Container>
     </>
---
+++ react/rc07-nba-legends/src/components/PlayerCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+const PlayerCard = ({ name, img, statistics }) => {
+  return (
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={img} height="350px" />
+        <ul>
+          {statistics.map((item, i) => (
+            <li className="list-unstyled h5 text-start" key={i}>
+              {" "}
+              🏀 {item}
+            </li>
+          ))}
+        </ul>
+        <Card.Footer>
+          <Card.Title>{name}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
+
+export default PlayerCard;
