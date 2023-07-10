84f5be1c4ccc25397437100b9b094b9f9e822880
KamilArici <kamilarici5@gmail.com>
2023-07-10 23:08:54 +0300

    card ternary

--- react/rc07-nba-legends/src/components/PlayerCard.jsx
+++ react/rc07-nba-legends/src/components/PlayerCard.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+  // && || ?:
   return (
     <Col>
-      <Card>
-        <Card.Img variant="top" src={img} height="350px" />
-        <ul>
-          {statistics.map((item, i) => (
-            <li className="list-unstyled h5 text-start" key={i}>
-              {" "}
-              🏀 {item}
-            </li>
-          ))}
-        </ul>
+      <Card onClick={() => setShowImage(!showImage)}>
+        {showImage ? (
+          <Card.Img
+            variant="top"
+            src={img}
+            height="350px"
+            // onClick={() => setShowImage(false)}
+          />
+        ) : (
+          <ul
+          //   onClick={() => setShowImage(true)}
+          >
+            {statistics.map((item, i) => (
+              <li className="list-unstyled h5 text-start" key={i}>
+                {" "}
+                🏀{item}
+              </li>
+            ))}
+          </ul>
+        )}
         <Card.Footer>
           <Card.Title>{name}</Card.Title>
         </Card.Footer>
