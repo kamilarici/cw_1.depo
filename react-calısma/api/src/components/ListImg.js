3a7158d30efa23a0f682626878ff1cdae455f6d1
KamilArici <kamilarici5@gmail.com>
2023-07-10 15:27:26 +0300

    css

--- react-calısma/api/src/components/ListImg.js
+++ react-calısma/api/src/components/ListImg.js
@@ -2,7 +2,7 @@ import ImageItem from "./ImageItem";
 
 const ListImg = ({ imagesPlaceholder }) => {
   return (
-    <div>
+    <div className="car">
       {imagesPlaceholder.map((image, index) => {
         return <ImageItem key={index} image={image} />;
       })}
