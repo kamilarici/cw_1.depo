45cd37c7006ea3ca542eb85f0ee7e09b4a2f0a4c
KamilArici <kamilarici5@gmail.com>
2023-07-10 21:42:35 +0300

    2

--- react/rc07-nba-legends/src/components/ContainerCard.jsx
+++ react/rc07-nba-legends/src/components/ContainerCard.jsx
@@ -1,9 +1,20 @@
+import React from "react";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const ContainerCard = () => {
   return (
     <>
-      <Form.Control type="search" placeholder="search player..." />
+      <Form.Control type="search" placeholder="Search Player..." />
+      <Container className="p-3 rounded-4 card-container">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+          <Col>3 of 2</Col>
+        </Row>
+      </Container>
     </>
   );
 };
