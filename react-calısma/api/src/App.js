2d8318017672f5c2cab27edefd553e38dabfceff
KamilArici <kamilarici5@gmail.com>
2023-07-10 12:17:54 +0300

    api2

--- react-calısma/api/src/App.js
+++ react-calısma/api/src/App.js
@@ -1,9 +1,14 @@
+import "./app.css";
+
+import SearchHeader from "./SearchHeader";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <SearchHeader />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-calısma/api/src/SearchHeader.jsx
@@ -0,0 +1,12 @@
+const SearchHeader = () => {
+  return (
+    <div>
+      <form>
+        <label>ne arıyorsunuz?</label>
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default SearchHeader;
