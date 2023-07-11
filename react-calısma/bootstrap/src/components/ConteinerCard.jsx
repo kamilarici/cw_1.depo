16d3870ceb129822a3406c431f1d5e84ab070c3d
KamilArici <kamilarici5@gmail.com>
2023-07-11 10:56:17 +0300

    row-col

--- react-calısma/bootstrap/src/components/ConteinerCard.jsx
+++ react-calısma/bootstrap/src/components/ConteinerCard.jsx
@@ -1,10 +1,25 @@
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const ConteinerCard = () => {
   return (
     <>
       {/* //! input içine çarpi ikonu getirmek için type:search yapmak gerekiyor.html getiriyor */}
       <Form.Control type="search" placeholder="Sarch Player" />
+      <Container>
+        <Row xs={2} md={4} lg={6}>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+          <Col>3 of 2</Col>
+          <Col>3 of 2</Col>
+          <Col>3 of 2</Col>
+          <Col>3 of 2</Col>
+          <Col>3 of 2</Col>
+          <Col>3 of 2</Col>
+        </Row>
+      </Container>
     </>
   );
 };
