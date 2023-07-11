4bf84f7e20ffccde8ea22fb643f5090b43bf1050
KamilArici <kamilarici5@gmail.com>
2023-07-11 12:38:00 +0300

    start

--- react-calısma/005-Appointment-App/src/App.js
+++ react-calısma/005-Appointment-App/src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
