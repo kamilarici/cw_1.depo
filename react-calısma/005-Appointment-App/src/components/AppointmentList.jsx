edefbf54f56c94de803ccaa381f7b3c59d3fda4e
KamilArici <kamilarici5@gmail.com>
2023-07-12 00:19:02 +0300

    modal

--- react-calısma/005-Appointment-App/src/components/AppointmentList.jsx
+++ react-calısma/005-Appointment-App/src/components/AppointmentList.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-const AppointmentList = () => {
+import React, { useState } from "react";
+const AppointmentList = ({ item, name }) => {
+  const [isConsulted, setisConsulted] = useState(false);
+  console.log(item);
   return (
-    <div className="d-flex justify-content-between border border-success rounded-3 p-2">
-      <div>
-        <h4>ads</h4>
-        <h5>fsfs</h5>
-      </div>
-      <div>
-        <h5>time</h5>
-        <h6>sfsdf</h6>
-      </div>
-      <div>
-        <button type="button" className="btn-close danger" aria-label="Close" />
+    <div
+      className="d-flex justify-content-between border border-success rounded-3 p-2"
+      onDoubleClick={() => setisConsulted(!isConsulted)}
+    >
+      <div className="relative">
+        {isConsulted ? (
+          <div className=" bg-danger absolite">consulted</div>
+        ) : null}
+        <div>
+          <h4>{item.patientName}</h4>
+          <h5>dr. {name}</h5>
+        </div>
+        <div>
+          <h5>time</h5>
+          <h6>{item.dataTime}</h6>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn-close danger"
+            aria-label="Close"
+          />
+        </div>
       </div>
     </div>
   );
