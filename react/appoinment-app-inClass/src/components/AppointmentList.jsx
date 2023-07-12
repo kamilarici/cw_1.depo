349da0908f4ee35f5d08ae021c11c3403c2ca39c
KamilArici <kamilarici5@gmail.com>
2023-07-13 00:01:28 +0300

    day

--- react/appoinment-app-inClass/src/components/AppointmentList.jsx
+++ react/appoinment-app-inClass/src/components/AppointmentList.jsx
@@ -6,20 +6,30 @@ import { TiDelete } from "react-icons/ti";
 const AppointmentList = ({ apps, setApps }) => {
   console.log(apps);
 
-  const handleDelete = (e) => {
-    // setApps();
-    console.log(e.target);
+  const handleDelete = (id) => {
+    setApps(apps.filter((item) => item.id !== id));
   };
+
+  const handleDoubleClick = (id) => {
+    setApps(
+      apps.map((item) =>
+        item.id == id ? { ...item, consulted: !item.consulted } : item
+      )
+    );
+  };
+  console.log(apps);
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
-        Appointment Listt
+        Appointment List
       </h3>
+      {apps.length < 1 && <img src="./img/appointment.jpg" width="70%" />}
 
       {apps.map(({ id, patient, consulted, doctor, day }) => (
         <div
           key={id}
           className={consulted ? "appointments consulted" : "appointments"}
+          onDoubleClick={() => handleDoubleClick(id)}
         >
           <Row className="justify-content-between align-items-center">
             <Col xs={12} sm={12} md={6}>
@@ -27,13 +37,13 @@ const AppointmentList = ({ apps, setApps }) => {
               <h5>{doctor}</h5>
             </Col>
             <Col>
-              <h5>day</h5>
+              <h5>date</h5>
             </Col>
             <Col className="text-end">
               <TiDelete
                 className="text-danger fs-1"
                 type="button"
-                onClick={() => handleDelete()}
+                onClick={() => handleDelete(id)}
               />
             </Col>
           </Row>
