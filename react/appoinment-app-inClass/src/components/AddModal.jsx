dfb2594b5c996f57a39c824a67b957ee35367037
KamilArici <kamilarici5@gmail.com>
2023-07-12 22:26:46 +0300

    handleclose

--- react/appoinment-app-inClass/src/components/AddModal.jsx
+++ react/appoinment-app-inClass/src/components/AddModal.jsx
@@ -1,8 +1,16 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
-
+import { useState } from "react";
 function AddModal({ show, handleClose }) {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    handleClose();
+  };
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -10,15 +18,25 @@ function AddModal({ show, handleClose }) {
           <Modal.Title>Appointment for ...</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="date">
               <Form.Label>Date</Form.Label>
-              <Form.Control type="date" placeholder="Date" />
+              <Form.Control
+                type="date"
+                placeholder="Date"
+                onChange={(e) => setDate(e.target.value)}
+                value={date}
+              />
             </Form.Group>
 
             <div className="text-center">
