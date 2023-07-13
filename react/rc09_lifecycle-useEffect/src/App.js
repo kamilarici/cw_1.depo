9e92368d21368c92b92b961aeb15d271d1c6ad18
KamilArici <kamilarici5@gmail.com>
2023-07-13 21:14:22 +0300

    3.

--- react/rc09_lifecycle-useEffect/src/App.js
+++ react/rc09_lifecycle-useEffect/src/App.js
@@ -1,11 +1,17 @@
-import LifeCycleMethods from "./components/LifeCycleMethods"
+import LifeCycleMethods from "./components/LifeCycleMethods";
+import { useState } from "react";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      <button className="btn btn-danger" onClick={() => setVisible(!visible)}>
+        Show/Hide
+      </button>
+
+      {visible && <LifeCycleMethods />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- react/rc09_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ react/rc09_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -16,23 +16,44 @@
 //* (Optional) Bir componentin guncellenmesinin hemen sonrasi (componentDidUpdate)
 //* Bir component'in DOM agacindan kaldirilmasi sonrasi(componentWillUnmount)
 
-import React from "react"
+import React from "react";
 
 class LifeCycleMethods extends React.Component {
+  //! 1-) Bir componentin olusturulmasinda cagrilir
   constructor(props) {
-    super(props)
+    console.log("1 - constructor run");
+    super(props);
     this.state = {
       count: 0,
-    }
+    };
+  }
+
+  //! 3-) Bir component DOM agacina eklendiginde calistirilir.
+  //! (İlk render sonrasi).
+  //! Her yasam dongusu icin bir kere calisir.
+  componentDidMount() {
+    //! Fetch, timer kurmak, network istegi
+    console.log("3 - componentDidMount");
+  }
+
+  //! 4-) Bu metot ilk render haric diger tüm render'lardan sonra cagrilir.
+  componentDidUpdate() {
+    console.log("4 - componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("5 - componentWillUnmount");
   }
 
   handleInc = () => {
     this.setState({
       count: this.state.count + 1,
-    })
-  }
+    });
+  };
 
+  //! 2-) Her bir state yada prop degistiginde (render) cagrilir
   render() {
+    console.log("2 - render run");
     return (
       <div className="container text-center">
         <h1 className="text-danger">LIFECYCLE METHODS</h1>
@@ -41,7 +62,7 @@ class LifeCycleMethods extends React.Component {
           INC
         </button>
       </div>
-    )
+    );
   }
 }
-export default LifeCycleMethods
+export default LifeCycleMethods;
