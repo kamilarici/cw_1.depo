90b23fcb404e9856f0a72912d7fe16c114f55966
KamilArici <kamilarici5@gmail.com>
2023-07-13 13:49:52 +0300

    test

--- react-calısma/useEffect/src/components/Test.jsx
+++ react-calısma/useEffect/src/components/Test.jsx
@@ -1,22 +1,36 @@
 import { useEffect, useState } from "react";
 
 const Test = () => {
-  const [count, setCount] = useState(0);
+  const [postId, setPostId] = useState(1);
+  const [post, setPost] = useState(false);
 
   useEffect(() => {
     console.log("component ilk yüklendiğinde");
-    return () => console.log("component destoreyed");
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/todos/1`)
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+    let interval = setInterval(() => console.log("interval çalıştı"), 1000);
+    return () => {
+      console.log("component destoreyed");
+      clearInterval(interval);
+    };
   }, []);
   useEffect(() => {
     console.log("component render oldu");
   });
   useEffect(() => {
-    console.log("count değeri değişti:", { count });
-  }, [count]);
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [postId]);
+
   return (
     <div>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>artır</button>
+      <h3>{postId}</h3>
+      {post && JSON.stringify(post)}
+      <br />
+      <button onClick={() => setPostId(postId + 1)}>artır</button>
       <h1>conponent gösterildi</h1>
     </div>
   );
