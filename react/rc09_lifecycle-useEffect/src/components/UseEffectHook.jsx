44af6beb70432231afc57d96f05a937a3866a44b
KamilArici <kamilarici5@gmail.com>
2023-07-13 21:23:28 +0300

    useEffect

---
+++ react/rc09_lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -0,0 +1,37 @@
+//?===============================================
+//?                USEEFFECT HOOK
+//?===============================================
+//! UseEffect Hook'u fonksiyonel componenler'te yan etkileri
+//! (side effect) gerceklestirmek icin kullanilir.
+//! componentDidMount,componentDidUpdate,ve componentWillUnmount
+//! metotlarinin bir birlesimi gibi dusunulebilir.
+
+//! useEffect(() => {
+//*   /* ComponentDidMount code */
+//*      Kodlar
+//! }, []);
+
+//! useEffect(() => {
+//*   */ ComponentDidMount + componentDidUpdate code */
+//! }, [state1, state2]);
+
+//! useEffect(() => {
+//?   /* ComponentDidMount code */
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, []);
+
+//! useEffect(() => {
+//*   //* componentDidMount code + componentDidUpdate code */
+
+//!   return () => {
+//*     //* componentWillUnmount code */
+//!   };
+//! }, [state1, state2]); //? Dependency Array
+
+const UseEffectHook = () => {
+  return <div>UseEffectHook</div>;
+};
+
+export default UseEffectHook;
