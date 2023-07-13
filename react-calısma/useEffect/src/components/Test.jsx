43fea7dcb7b334d942488e44cc82b683de58fc32
KamilArici <kamilarici5@gmail.com>
2023-07-13 10:59:09 +0300

    useeffect

--- react-calısma/useEffect/src/components/Test.jsx
+++ react-calısma/useEffect/src/components/Test.jsx
@@ -1,14 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Test = () => {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
     console.log("component ilk yüklendiğinde");
+    return () => console.log("component destoreyed");
   }, []);
   useEffect(() => {
     console.log("component render oldu");
   });
+  useEffect(() => {
+    console.log("count değeri değişti:", { count });
+  }, [count]);
   return (
     <div>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>artır</button>
       <h1>conponent gösterildi</h1>
     </div>
   );
