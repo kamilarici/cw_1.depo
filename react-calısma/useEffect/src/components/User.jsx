5cc7c15174d203a2e067a96fe3a7ccc7fc997af6
KamilArici <kamilarici5@gmail.com>
2023-07-14 19:08:10 +0300

    useEffect(() => {  end

--- react-calısma/useEffect/src/components/User.jsx
+++ react-calısma/useEffect/src/components/User.jsx
@@ -1,35 +1,49 @@
 import React, { useEffect, useState } from "react";
 
 const User = () => {
-  const [userData, setUserData] = useState("");
+  // let userData = "deneme"
+
+  const [userData, setUserData] = useState();
+
   const getUser = () => {
     fetch("https://randomuser.me/api")
       .then((res) => res.json())
       .then((data) => setUserData(data.results[0]))
       .catch((err) => console.log(err));
-    console.log(userData);
   };
+
   useEffect(() => {
     // fetch("https://randomuser.me/api")
     //   .then((res) => res.json())
     //   .then((data) => setUserData(data.results[0]))
-    //   .catch((err) => console.log(err));
+    //   .catch((err) => console.log(err))
+    //! ComponentDidMount
+    // const timer = setInterval(getUser, 3000);
     getUser();
+
+    //! ComponentWillUnMount
+    // return () => {
+    //   clearInterval(timer);
+    // };
   }, []);
 
+  console.log(userData);
   return (
     <div>
-      <img src={userData?.picture?.large} alt="" />
-      <h4>hi my name is </h4>
+      {/* Optional Chaining */}
+      <img src={userData?.picture?.large} className="rounded-circle" alt="" />
+      <h4>Hi, My Name is</h4>
       <h1>
-        {userData?.name?.first}
-        {userData?.name?.last}
+        {userData?.name?.first} {userData?.name?.last}
       </h1>
       <h3>{userData?.email}</h3>
       <h4>{new Date(userData?.dob?.date).toLocaleDateString()}</h4>
       <h5>{userData?.phone}</h5>
-      {/* <h6>{userData?.location.city} </h6> */}
-      <button onClick={getUser}>get user</button>
+      <h6>{userData?.location?.city}</h6>
+
+      <button className="btn btn-success" onClick={getUser}>
+        Get Random User
+      </button>
     </div>
   );
 };
