776a1e3e1531e4b35b95c7e70d638d4ebe8b6eff
KamilArici <kamilarici5@gmail.com>
2023-07-15 22:14:49 +0300

    props

--- react-calısma/form-2/src/App.js
+++ react-calısma/form-2/src/App.js
@@ -1,16 +1,26 @@
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const createTask = (title, taskDesc) => {
-    console.log(title, taskDesc);
+    const createdTasks = [
+      ...tasks,
+      {
+        id: Math.round(Math.random() * 99999),
+        title: title,
+        taskDesc: taskDesc,
+      },
+    ];
+    setTasks(createdTasks);
   };
   return (
     <div className="App">
       <TaskCreate onCreate={createTask} />
       <h1>Görevler</h1>
-      <TaskList />
+      <TaskList task={tasks} />
     </div>
   );
 }
