ad06f6253cc392cddc716193159f10e1ae51ecca
KamilArici <kamilarici5@gmail.com>
2023-07-15 21:30:07 +0300

    e.target.value

--- react-calısma/form-2/src/components/TaskCreate.jsx
+++ react-calısma/form-2/src/components/TaskCreate.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import { useState } from "react";
 
 const TaskCreate = () => {
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDesk] = useState("");
+  console.log(title, taskDesc);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleTaskChange = (e) => {
+    setTaskDesk(e.target.value);
+  };
   return (
     <div className="task-create">
       <h3>Lütfen task ekleyiniz</h3>
       <form className="task-form">
         <label className="task-label">başlık</label>
-        <input className="task-input" type="text" />
+        <input
+          value={title}
+          onChange={handleChange}
+          className="task-input"
+          type="text"
+        />
         <label className="task-label">Task giriniz</label>
-        <textarea className="task-input" />
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskChange}
+          className="task-input"
+          rows={5}
+        />
         <button className="task-button">Oluştur</button>
       </form>
     </div>
