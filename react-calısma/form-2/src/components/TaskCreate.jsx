869145019352c96bb938d6ec4e0d1545010e5e29
KamilArici <kamilarici5@gmail.com>
2023-07-15 21:19:40 +0300

    form-create

--- react-calısma/form-2/src/components/TaskCreate.jsx
+++ react-calısma/form-2/src/components/TaskCreate.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const TaskCreate = () => {
   return (
-    <div>
+    <div className="task-create">
       <h3>Lütfen task ekleyiniz</h3>
       <form className="task-form">
-        <label>başlık</label>
-        <input type="text" />
-        <label>Task giriniz</label>
-        <textarea />
-        <button>Oluştur</button>
+        <label className="task-label">başlık</label>
+        <input className="task-input" type="text" />
+        <label className="task-label">Task giriniz</label>
+        <textarea className="task-input" />
+        <button className="task-button">Oluştur</button>
       </form>
     </div>
   );
