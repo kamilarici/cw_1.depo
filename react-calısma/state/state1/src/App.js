2f3338c687fe7be7aa0cc0cf603d6d4e21a752cc
KamilArici <kamilarici5@gmail.com>
2023-07-16 15:27:32 +0300

    context finished

--- react-calısma/state/state1/src/App.js
+++ react-calısma/state/state1/src/App.js
@@ -4,15 +4,16 @@ import Auth from "./Auth";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [authStatus, setAuthStatus] = useState(false);
 
   const handlClick = () => {
     setValue(value + 1);
   };
   const loginAuth = () => {
-    console.log("metodu tetikledim");
+    setAuthStatus(true);
   };
   return (
-    <AuthContext.Provider value={{ status: false, login: loginAuth }}>
+    <AuthContext.Provider value={{ status: authStatus, login: loginAuth }}>
       <Auth />
     </AuthContext.Provider>
   );
