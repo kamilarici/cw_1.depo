0269225218e5be8505e9bfbdca4100422fc0f6f0
KamilArici <kamilarici5@gmail.com>
2023-07-19 15:08:08 +0300

    3.

---
+++ react-calısma/router/src/components/History.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const History = () => {
+  return (
+    <div>
+      <div>History</div>
+      <nav>
+        <Link to="compony">our company</Link>
+        <Link>mananegment</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default History;
