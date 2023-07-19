1a78077f4309727b08a127738e8e55f7ac8d3fbc
KamilArici <kamilarici5@gmail.com>
2023-07-19 21:27:49 +0300

    useNavigate

--- react/router-intro-inClass/src/pages/Contact.jsx
+++ react/router-intro-inClass/src/pages/Contact.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const Contact = () => {
+  const navigate = useNavigate();
   return (
     <div className="p-3">
       <h1>Contact</h1>
+      <button className="btn btn-success" onClick={() => navigate(-1)}>
+        go back
+      </button>
+      <button className="btn btn-danger" onClick={() => navigate("/")}>
+        go back
+      </button>
     </div>
   );
 };
