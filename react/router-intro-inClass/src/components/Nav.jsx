71704f2aae7b6241480b501538d73cd5748940c0
KamilArici <kamilarici5@gmail.com>
2023-07-19 21:06:14 +0300

    active

--- react/router-intro-inClass/src/components/Nav.jsx
+++ react/router-intro-inClass/src/components/Nav.jsx
@@ -1,14 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+//? Link ve a tagi
+//* Single page application uygulamarında temel mantık aynı sayfada sayfa yenilenmeden istenilen komponentin sayfaya çağırılmasıdır.
+//* SPA da projenin navigasyonunda gezerken her linke tıklandığında sayfanın kendini yenilemesi durumu varsa bu bir problemdir. Bunun nedeni ise linke tıklandığında çağırılan komponentin
+//* <a href="/path"></a> şeklinde çağırılmış olmasıdır. a tagi servera istek attığı için sayfayı yeniler.
+//* Eğer a etiketi yerine react-router-dom elementi olan Link componenti kullanılırsa
+//* <Link to="/path"></Link>
+//* sayfa yenileme problemi ortadan kalkacaktır. Artık reload işlemi olmadan gezinme yapılabilecektir.
+//* Bu kullanım sadece React özelinde değil diğer SPA uygulamarı içinde geçerlidir.
+// BrowserRouter ==> dede
+// Routes ==> baba
+// Route==> çocuk
+// Nav-Footer ==> amca (yeğenler amcalarını görür)
 const Nav = () => {
   return (
     <ul className="bg-warning">
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        {/* <NavLink
+          style={({ isActive }) => ({ color: isActive && "red" })}
+          to="/instructors"
+        >
+          Instructors
+        </NavLink> */}
+        {/* //? Navlink kullanıldığında aktif olan link elementine otomatik active class'ı eklenir, bu class stillendirme için kullanılabilir */}
+        <NavLink to="/instructors">Instructors</NavLink>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
