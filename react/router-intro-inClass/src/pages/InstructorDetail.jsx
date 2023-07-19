49be9fe2cddc032b0ea19b36e73f6af87449b653
KamilArici <kamilarici5@gmail.com>
2023-07-19 22:05:02 +0300

    params

--- react/router-intro-inClass/src/pages/InstructorDetail.jsx
+++ react/router-intro-inClass/src/pages/InstructorDetail.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 
-const InstructerDetail = () => {
-  return <div>InstructerDetail</div>;
+const InstructorDetail = () => {
+  //   let param = useParams();
+  //* Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
+  //* yakalanan id'ye ait güncel veriler fetch yapılabilir
+  let { id } = useParams();
+  console.log({ id });
+  //   let location = useLocation();
+  let { state, pathname } = useLocation();
+  console.log({ state });
+  return (
+    <div className="p-3">
+      <p>
+        from useParam: I'am instructor-
+        <span className="text-danger fw-bolder">{id}</span>
+      </p>
+      <p>
+        from useLocation: I'am instructor-
+        <span className="text-danger fw-bolder">{state?.name}</span>
+      </p>
+      <p>our path is: {pathname}</p>
+    </div>
+  );
 };
 
-export default InstructerDetail;
+export default InstructorDetail;
