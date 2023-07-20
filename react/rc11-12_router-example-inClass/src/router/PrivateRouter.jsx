f23e01424539a8025ca4eba91e096cb9bf2c1208
KamilArici <kamilarici5@gmail.com>
2023-07-20 22:32:41 +0300

    PrivateRouter true

--- react/rc11-12_router-example-inClass/src/router/PrivateRouter.jsx
+++ react/rc11-12_router-example-inClass/src/router/PrivateRouter.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 
 const PrivateRouter = () => {
-  const user = false;
+  const user = true;
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
