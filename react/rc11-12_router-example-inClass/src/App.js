36c35fc533163a7951724fd9195e2970a8437c14
KamilArici <kamilarici5@gmail.com>
2023-07-20 21:22:39 +0300

    son

--- react/rc11-12_router-example-inClass/src/App.js
+++ react/rc11-12_router-example-inClass/src/App.js
@@ -1,26 +1,30 @@
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import People from "./pages/People";
-import NotFound from "./pages/NotFound";
-import { Route, Routes } from "react-router-dom";
-import Paths from "./pages/Paths";
-import PersonDetail from "./pages/PersonDetail";
+// import Footer from "./components/Footer";
+// import Nav from "./components/Nav";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import People from "./pages/People";
+// import NotFound from "./pages/NotFound";
+// import { Route, Routes } from "react-router-dom";
+// import Paths from "./pages/Paths";
+// import PersonDetail from "./pages/PersonDetail";
+
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <>
-      <Nav />
+      <AppRouter />
+      {/* //?router yapısını ayrı bir component haline getirip router dosyasına taşıdık */}
+      {/* <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/paths" element={<Paths />} />
-        <Route path="/people/" element={<People />} />
+        <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
