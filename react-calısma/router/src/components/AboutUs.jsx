0318a61b0b7c4b50f8fed870c8d1465a65976170
KamilArici <kamilarici5@gmail.com>
2023-07-20 11:32:32 +0300

    lazy

--- react-calısma/router/src/components/AboutUs.jsx
+++ react-calısma/router/src/components/AboutUs.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+      praesentium repudiandae suscipit, minus dolorum vero harum mollitia iste
+      aspernatur aliquid, reiciendis magnam, ratione asperiores temporibus porro
+      obcaecati velit itaque veritatis neque amet ipsam? Non, dolores molestiae
+      quisquam illum tenetur dignissimos laboriosam quam dolorum numquam,
+      voluptatem corrupti obcaecati delectus reiciendis quo? Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Debitis praesentium repudiandae
+      suscipit, minus dolorum vero harum mollitia iste aspernatur aliquid,
+      reiciendis magnam, ratione asperiores temporibus porro obcaecati velit
+      itaque veritatis neque amet ipsam? Non, dolores molestiae quisquam illum
+      tenetur dignissimos laboriosam quam dolorum numquam, voluptatem corrupti
+      obcaecati delectus reiciendis quo? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis praesentium repudiandae suscipit, minus dolorum
+      vero harum mollitia iste aspernatur aliquid, reiciendis magnam, ratione
+      asperiores temporibus porro obcaecati velit itaque veritatis neque amet
+      ipsam? Non, dolores molestiae quisquam illum tenetur dignissimos
+      laboriosam quam dolorum numquam, voluptatem corrupti obcaecati delectus
+      reiciendis quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Debitis praesentium repudiandae suscipit, minus dolorum vero harum
+      mollitia iste aspernatur aliquid, reiciendis magnam, ratione asperiores
+      temporibus porro obcaecati velit itaque veritatis neque amet ipsam? Non,
+      dolores molestiae quisquam illum tenetur dignissimos laboriosam quam
+      dolorum numquam, voluptatem corrupti obcaecati delectus reiciendis quo?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+      praesentium repudiandae suscipit, minus dolorum vero harum mollitia iste
+      aspernatur aliquid, reiciendis magnam, ratione asperiores temporibus porro
+      obcaecati velit itaque veritatis neque amet ipsam? Non, dolores molestiae
+      quisquam illum tenetur dignissimos laboriosam quam dolorum numquam,
+      voluptatem corrupti obcaecati delectus reiciendis quo? Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Debitis praesentium repudiandae
+      suscipit, minus dolorum vero harum mollitia iste aspernatur aliquid,
+      reiciendis magnam, ratione asperiores temporibus porro obcaecati velit
+      itaque veritatis neque amet ipsam? Non, dolores molestiae quisquam illum
+      tenetur dignissimos laboriosam quam dolorum numquam, voluptatem corrupti
+      obcaecati delectus reiciendis quo? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Debitis praesentium repudiandae suscipit, minus dolorum
+      vero harum mollitia iste aspernatur aliquid, reiciendis magnam, ratione
+      asperiores temporibus porro obcaecati velit itaque veritatis neque amet
+      ipsam? Non, dolores molestiae quisquam illum tenetur dignissimos
+      laboriosam quam dolorum numquam, voluptatem corrupti obcaecati delectus
+      reiciendis quo? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Debitis praesentium repudiandae suscipit, minus dolorum vero harum
+      mollitia iste aspernatur aliquid, reiciendis magnam, ratione asperiores
+      temporibus porro obcaecati velit itaque veritatis neque amet ipsam? Non,
+      dolores molestiae quisquam illum tenetur dignissimos laboriosam quam
+      dolorum numquam, voluptatem corrupti obcaecati delectus reiciendis quo?
+    </div>
+  );
 };
 
 export default AboutUs;
