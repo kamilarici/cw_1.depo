3cc8a6b3d4ed5726e10e4a5f9c7725b51a4d2a1c
KamilArici <kamilarici5@gmail.com>
2023-07-20 11:45:43 +0300

    lazy

--- react-calısma/router/src/App.js
+++ react-calısma/router/src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import AboutUs from "./components/AboutUs";
+// import AboutUs from "./components/AboutUs";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Mission from "./components/Mission";
@@ -10,14 +10,22 @@ import Company from "./components/Company";
 import Team from "./components/Team";
 import Members from "./components/Members";
 import MemberDetail from "./components/MemberDetail";
-
+import React from "react";
+const LazyAboutUs = React.lazy(() => import("./components/AboutUs"));
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route
+          path="/aboutUs"
+          element={
+            <React.Suspense>
+              <LazyAboutUs />
+            </React.Suspense>
+          }
+        />
         <Route path="/mission" element={<Mission />} />
         <Route path="/history" element={<History />}>
           <Route path="company" element={<Company />} />
