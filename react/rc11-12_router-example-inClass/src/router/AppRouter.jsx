95c15150195d97152d52e7e3902508d38e64d4e1
KamilArici <kamilarici5@gmail.com>
2023-07-20 21:20:35 +0300

    router

---
+++ react/rc11-12_router-example-inClass/src/router/AppRouter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Nav from "../components/Nav";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Paths from "../pages/Paths";
+import People from "../pages/People";
+import PersonDetail from "../pages/PersonDetail";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+import Footer from "../components/Footer";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/paths" element={<Paths />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default AppRouter;
