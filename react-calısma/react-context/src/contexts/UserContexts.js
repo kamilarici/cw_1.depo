2113a61291f1d8b7612a8360c1933ab432e450b2
KamilArici <kamilarici5@gmail.com>
2023-07-23 21:51:11 +0300

    PruducContext

--- react-calısma/react-context/src/contexts/UserContexts.js
+++ react-calısma/react-context/src/contexts/UserContexts.js
@@ -1,23 +1,21 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 
-const UserContexts = createContext()
+const UserContexts = createContext();
 
-export const  UserProvider = ({children}) => {
-    const [name, setName] = useState("ali")
-    const [surname, setSurname] = useState("durul")
+export const UserProvider = ({ children }) => {
+  const [name, setName] = useState("ali");
+  const [surname, setSurname] = useState("durul");
 
-    const values = {
-        name,
-        setName,
-        surname,
-        setSurname
-    }
+  const values = {
+    name,
+    setName,
+    surname,
+    setSurname,
+  };
 
-  return(
+  return (
     <UserContexts.Provider value={values}>{children}</UserContexts.Provider>
-  )
-}
-
-
+  );
+};
 
 export default UserContexts;
