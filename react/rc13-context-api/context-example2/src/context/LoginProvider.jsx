ac08f56f8405dcb3862e624317dc6c5be89c7320
KamilArici <kamilarici5@gmail.com>
2023-07-24 01:26:58 +0300

    consuming

--- react/rc13-context-api/context-example2/src/context/LoginProvider.jsx
+++ react/rc13-context-api/context-example2/src/context/LoginProvider.jsx
@@ -1,26 +1,28 @@
-import { createContext, useState, useContext } from "react"
+import { createContext, useState, useContext } from "react";
 
 //! 1- Login Context'i olusuturuldu
-const LoginContext = createContext()
+const LoginContext = createContext();
 
 //! 2-Sarmalayici (Provider) Component
 const LoginProvider = ({ children }) => {
   // //! Local State
-  const [user, setUser] = useState({ email: "", password: "" })
+  const [user, setUser] = useState({ email: "", password: "" });
 
   const values = {
     user,
     setUser,
-  }
+  };
 
   return (
     <LoginContext.Provider value={values}>{children}</LoginContext.Provider>
-  )
-}
+  );
+};
 
 //! 3- consuming custom hook
-export const useLoginContext = () => {
-  return useContext(LoginContext)
-}
+//? burada kendi hookumuzu yazdık 2 işlemi tek işleme düşürdük
 
-export default LoginProvider
+export const useLoginContext = () => {
+  return useContext(LoginContext);
+};
+
+export default LoginProvider;
