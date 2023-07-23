b7c8b042e84a18804291d4cd3d1285c01fec7fb6
KamilArici <kamilarici5@gmail.com>
2023-07-23 21:42:07 +0300

    PROVİDER

---
+++ react-tw/tw-11-pruductlist/src/context/ProducContext.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+const PruducContext=createContext()
+
+export  const ProviderContext=()=>{
+
+
+
+
+    return(
+
+    )
+}
+
+export default PruducContext;
