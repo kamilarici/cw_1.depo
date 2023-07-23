031ad68449596d1cf3d24ae72d73fcc332f076eb
KamilArici <kamilarici5@gmail.com>
2023-07-24 00:00:05 +0300

    edit10

--- react-calısma/react-context/src/components/A.jsx
+++ react-calısma/react-context/src/components/A.jsx
@@ -1,19 +1,25 @@
-import React, { useContext } from 'react'
-import B from './B'
-import UserContexts from '../contexts/UserContexts'
+import React, { useContext } from "react";
+import B from "./B";
+import UserContexts from "../contexts/UserContexts";
 
 const A = () => {
-    const { setName , setSurname} = useContext(UserContexts)
-    return (
-        <div>
-            <h1>A</h1>
-       <button type='button' onClick={()=>{
-        setName("Veli")
-        setSurname("Deli")
-       }}>change name</button>
-            <B />
-        </div>
-    )
-}
+  //? context deki state ler baynı yerde çekilebilir
+  const { setName, setSurname } = useContext(UserContexts);
+  return (
+    <div>
+      <h1>A</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setName("Veli");
+          setSurname("Deli");
+        }}
+      >
+        change name
+      </button>
+      <B />
+    </div>
+  );
+};
 
-export default A
+export default A;
