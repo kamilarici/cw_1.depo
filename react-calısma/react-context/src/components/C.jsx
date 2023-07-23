8cc34a605ac3bf740b869d53e113e79788256d96
KamilArici <kamilarici5@gmail.com>
2023-07-23 21:20:04 +0300

    2.

--- react-calısma/react-context/src/components/C.jsx
+++ react-calısma/react-context/src/components/C.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { UserContext } from "../App";
 
 const C = () => {
   return (
     <div>
-      <h1>C</h1>
+      <UserContext.Consumer>
+        <h1>C</h1>
+        <h1>benim adım </h1>
+      </UserContext.Consumer>
     </div>
   );
 };
