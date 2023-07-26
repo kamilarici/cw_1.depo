6e064ab2ad58511c1483d0a3046ff6e425a42641
KamilArici <kamilarici5@gmail.com>
2023-07-26 21:04:09 +0300

    edit3

--- react/rc15_clarus-library/inclass/src/App.js
+++ react/rc15_clarus-library/inclass/src/App.js
@@ -1,10 +1,19 @@
-import AppRouter from './router/AppRouter';
+import AppRouter from "./router/AppRouter";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darktheme } from "./styles/theme";
+import { GlobalStyles } from "./styles/Global.styles";
+import { useThemeContext } from "./context/ThemeContext";
 
 function App() {
+  const { myTheme } = useThemeContext();
+  const themes = myTheme;
   return (
-    <div className="App">
-      <AppRouter/>
-    </div>
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyles />
+        <AppRouter />
+      </ThemeProvider>
+    </>
   );
 }
 
