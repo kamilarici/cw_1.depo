2d0cb82954bc59c6e0a54a16b40ee8bf3b424153
KamilArici <kamilarici5@gmail.com>
2023-07-27 15:24:14 +0300

    edit

--- react-proje/Kurslarım/src/App.js
+++ react-proje/Kurslarım/src/App.js
@@ -1,15 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import FilmYear from "./components/FilmYear";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />}>
-          <Route path="filmyear" element={<FilmYear />} />
-          <Route />
-        </Route>
+        <Route path="/" element={<Main />} />
+        <Route />
+        <Route />
+        <Route />
       </Routes>
     </div>
   );
---
+++ react-proje/Kurslarım/src/components/FilmInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilmInfo = () => {
+  return <div>FilmInfo</div>;
+};
+
+export default FilmInfo;
