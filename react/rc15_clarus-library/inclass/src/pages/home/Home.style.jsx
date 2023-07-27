de99dd107944ad4a52c4d02148fbcb90373f79e8
KamilArici <kamilarici5@gmail.com>
2023-07-27 20:31:49 +0300

    styled

--- react/rc15_clarus-library/inclass/src/pages/home/Home.style.jsx
+++ react/rc15_clarus-library/inclass/src/pages/home/Home.style.jsx
@@ -6,6 +6,15 @@ export const HomeContainer = styled(Flex)`
   flex-direction: column;
 `;
 
-export const HomeImage = styled.div``;
+export const HomeImage = styled.div`
+  width: 100%;
+  text-align: center;
+  margin: 2rem;
+  img {
+    width: 45%;
+    height: 400px;
+    border-radius: 1rem;
+  }
+`;
 
 export const CardContainer = styled(Flex)``;
