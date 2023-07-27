5430709c4b0ad5981c97a57ef8c742404f120617
KamilArici <kamilarici5@gmail.com>
2023-07-27 20:18:06 +0300

    card

--- react/rc15_clarus-library/inclass/src/pages/home/Home.jsx
+++ react/rc15_clarus-library/inclass/src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
+import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <HomeContainer>
+      <Header />
+      <HomeImage></HomeImage>
+      <CardContainer>
+        <Card />
+      </CardContainer>
+    </HomeContainer>
+  );
+};
 
-export default Home
+export default Home;
--- react/rc15_clarus-library/inclass/src/pages/home/Home.style.jsx
+++ react/rc15_clarus-library/inclass/src/pages/home/Home.style.jsx
@@ -1,1 +1,11 @@
 // Home Style
+import styled from "styled-components";
+import Flex from "../../styles/Flex";
+
+export const HomeContainer = styled(Flex)`
+  flex-direction: column;
+`;
+
+export const HomeImage = styled.div``;
+
+export const CardContainer = styled(Flex)``;
