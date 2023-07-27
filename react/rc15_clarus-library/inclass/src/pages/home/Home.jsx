90b234627b14c1a5ead94866eff52ac25a17e911
KamilArici <kamilarici5@gmail.com>
2023-07-27 22:27:42 +0300

    edit0

--- react/rc15_clarus-library/inclass/src/pages/home/Home.jsx
+++ react/rc15_clarus-library/inclass/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Card from "../../components/card/Card";
 import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
+import homeImg from "../../assets/books.jpg";
 
 const Home = () => {
   return (
