6c66e91e220830e8bea863145dd3e0ba450d21f0
KamilArici <kamilarici5@gmail.com>
2023-07-28 23:19:45 +0300

    CardMedia

--- react/rc15_clarus-library/starter/src/components/card/Card.jsx
+++ react/rc15_clarus-library/starter/src/components/card/Card.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+// Card Style
+import styled from "styled-components";
+import Flex from "../../styles/Flex";
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+export const CardContainer = styled(Flex)`
+  flex-direction: column;
+  height: 20rem;
+  width: 20rem;
+  padding: 0.5rem;
+  border-radius: 1rem;
+  margin: 0.7rem;
+  box-shadow: 3px 3px 10px 1px rgba(0, 0, 0, 0.3);
+  background-color: ${({ theme }) => theme.colors.navbarBgColor};
+`;
 
-export default Card
+export const CardHeader = styled.h2`
+  font-size: 1.2rem;
+  text-align: center;
+  overflow: hidden;
+  height: 5rem;
+`;
+
+export const CardMedia = styled.img`
+  min-height: 10rem;
+  max-height: 10rem;
+`;
+
+export const CardButton = styled.button`
+  font-weight: bold;
+  padding: 0.7rem;
+  outline: none;
+  border: none;
+  margin: 0.5rem;
+  border-radius: 3px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.6;
+  }
+  background-color: ${({ theme }) => theme.colors.mainColor};
+  color: ${({ theme }) => theme.colors.linkHoverColor};
+`;
