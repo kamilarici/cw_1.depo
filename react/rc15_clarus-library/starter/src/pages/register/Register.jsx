8620f2d758ac1c1a7b42dac580e85b49cd09cf13
KamilArici <kamilarici5@gmail.com>
2023-07-28 23:24:58 +0300

    RegisterContainer

--- react/rc15_clarus-library/starter/src/pages/register/Register.jsx
+++ react/rc15_clarus-library/starter/src/pages/register/Register.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import underConst from "../../assets/const.png";
+import { RegisterContainer, RegisterImage } from "./Register.style";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <RegisterContainer>
+      <RegisterImage src={underConst} alt="" />
+    </RegisterContainer>
+  );
+};
 
-export default Register
+export default Register;
