e35549b4032921c708e829fef2b177d7fc47a46c
KamilArici <kamilarici5@gmail.com>
2023-07-29 11:49:22 +0300

    edit

--- react/rc17-18_movie-app/tailwindcss-intro/src/App.js
+++ react/rc17-18_movie-app/tailwindcss-intro/src/App.js
@@ -1,8 +1,13 @@
-
 function App() {
   return (
-    <div>
-      <h1>TailwindCss Kurulum</h1>
+    <div className="bg-fuchsia-300 h-screen p-[10px]">
+      <h1 className="text-8xl bg-[#ededed]">TailwindCss Kurulum</h1>
+      <h1 className="text-3xl font-bold text-center underline">Hello world!</h1>
+
+      <div className="flex gap-3 mt-4">
+        <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+        <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+      </div>
     </div>
   );
 }
