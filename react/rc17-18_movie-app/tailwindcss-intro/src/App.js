c183a3c8f005022b725235147879f9395da5b8f4
KamilArici <kamilarici5@gmail.com>
2023-07-29 12:02:16 +0300

    tw

--- react/rc17-18_movie-app/tailwindcss-intro/src/App.js
+++ react/rc17-18_movie-app/tailwindcss-intro/src/App.js
@@ -6,7 +6,19 @@ function App() {
 
       <div className="flex gap-3 mt-4">
         <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
-        <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 h-[250px]"></div>
+        <div className="card">
+          <img
+            src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+            alt="sa"
+            className="rounded-xl "
+          />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Repellendus, fugiat? Eos, temporibus enim animi dolorem laudantium
+            aliquid delectus accusantium ratione, odio maiores excepturi
+            molestiae laboriosam?
+          </p>
+        </div>
       </div>
     </div>
   );
