422e2aa75077e39e1dca5858e24efcd98a21d6c0
KamilArici <kamilarici5@gmail.com>
2023-07-29 23:30:09 +0300

    edit

--- react/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
+++ react/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
@@ -2,21 +2,16 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Cards from "../components/Cards";
 const Main = () => {
+  const [movies, setMovies] = useState();
+  const [query, setQuery] = useState();
   const API_KEY = "b1ad040c8b59ee92603839b9fbbe4adf";
   const [apiUrl, setApiUrl] = useState(
-    `https://api.themoviedb.org/3/discover/movie?api_key=`
-    );
-    const [movies, setMovies] = useState();
-    const [query, setQuery] = useState();
-
-
-    
-    const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${}`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+  );
+  const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
   const getData = async () => {
     try {
-      const response = await axios(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
-      );
+      const response = await axios(`${apiUrl}`);
       setMovies(response?.data?.results);
     } catch (error) {
       console.log(error);
@@ -24,11 +19,10 @@ const Main = () => {
   };
   useEffect(() => {
     getData();
-  }, []);
+  }, [apiUrl]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    setQuery(e.target[0].value);
-    // setApiUrl(SEARCH_URL)
+    query && setApiUrl(SEARCH_URL);
   };
   return (
     <main>
@@ -41,6 +35,8 @@ const Main = () => {
           type="search"
           placeholder="Search A Movie.."
           className="outline-none border w-4/12 mr-3 text-black text-lg"
+          onChange={(e) => setQuery(e.target.value.trim())}
+          required
         />
         <button type="submit">Search</button>
       </form>
