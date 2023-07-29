0866abeed017201215bdab900431910dd99442c1
KamilArici <kamilarici5@gmail.com>
2023-07-29 23:28:00 +0300

    edit

--- react/rc17-18_movie-app/movie-app-inclass/src/components/Navbar.jsx
+++ react/rc17-18_movie-app/movie-app-inclass/src/components/Navbar.jsx
@@ -1,7 +1,101 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <header className={`flex items-center justify-center w-full bg-slate-700`}>
+      <div className="container ">
+        <div className="relative flex items-center justify-between -mx-4">
+          <div className="max-w-full px-4 w-60">
+            <a href="/#" className="block w-full py-5">
+              <img
+                src="https://cdn.tailgrids.com/1.0/assets/images/logo/logo.svg"
+                alt="logo"
+                className="w-full"
+              />
+            </a>
+          </div>
+          <div className="flex items-center justify-between w-full px-4 ">
+            <div className="me">
+              <button
+                // @click="navbarOpen = !navbarOpen"
+                onClick={() => setOpen(!open)}
+                // :className="navbarOpen && 'navbarTogglerActive' "
+                id="navbarToggler"
+                className={` ${
+                  open && "navbarTogglerActive"
+                } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
+              >
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color"></span>
+              </button>
+              <nav
+                // :className="!navbarOpen && 'hidden' "
+                id="navbarCollapse"
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white py-5 px-6 shadow lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none ${
+                  !open && "hidden"
+                } `}
+              >
+                <ul className="block lg:flex">
+                  <ListItem
+                    navItemStyles="text-dark hover:text-primary"
+                    NavLink="/#"
+                  >
+                    Home
+                  </ListItem>
+                  <ListItem
+                    navItemStyles="text-dark hover:text-primary"
+                    NavLink="/#"
+                  >
+                    Payment
+                  </ListItem>
+                  <ListItem
+                    navItemStyles="text-dark hover:text-primary"
+                    NavLink="/#"
+                  >
+                    About
+                  </ListItem>
+                  <ListItem
+                    navItemStyles="text-dark hover:text-primary"
+                    NavLink="/#"
+                  >
+                    Blog
+                  </ListItem>
+                </ul>
+              </nav>
+            </div>
+            <div className="justify-end hidden pr-16 sm:flex lg:pr-0">
+              <a
+                href="/#"
+                className="py-3 text-base font-medium px-7 text-dark hover:text-primary"
+              >
+                Sign in
+              </a>
+              <a
+                href="/#"
+                className="py-3 text-base font-medium text-white rounded-lg bg-primary px-7 hover:bg-opacity-90"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
-
 export default Navbar;
+const ListItem = ({ children, navItemStyles, NavLink }) => {
+  return (
+    <>
+      <li>
+        <a
+          href={NavLink}
+          className={`flex py-2 text-base font-medium lg:ml-12 lg:inline-flex ${navItemStyles}`}
+        >
+          {children}
+        </a>
+      </li>
+    </>
+  );
+};
--- react/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
+++ react/rc17-18_movie-app/movie-app-inclass/src/pages/Main.jsx
@@ -2,13 +2,22 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Cards from "../components/Cards";
 const Main = () => {
-  const API_KEY = "405cd9fb67e60c3e2feae1872a93ac53";
-  const API_URL = "https://api.themoviedb.org/3/discover/movie?api_key=";
-  const [movies, setMovies] = useState([]);
+  const API_KEY = "b1ad040c8b59ee92603839b9fbbe4adf";
+  const [apiUrl, setApiUrl] = useState(
+    `https://api.themoviedb.org/3/discover/movie?api_key=`
+    );
+    const [movies, setMovies] = useState();
+    const [query, setQuery] = useState();
+
+
+    
+    const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${}`
   const getData = async () => {
     try {
-      const response = await axios(`${API_URL}${API_KEY}`);
-      setMovies(response.data.results);
+      const response = await axios(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+      );
+      setMovies(response?.data?.results);
     } catch (error) {
       console.log(error);
     }
@@ -16,10 +25,18 @@ const Main = () => {
   useEffect(() => {
     getData();
   }, []);
-  console.log(movies);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(e.target[0].value);
+    // setApiUrl(SEARCH_URL)
+  };
   return (
     <main>
-      <form action="" className="flex justify-center p-2">
+      <form
+        action=""
+        className="flex justify-center p-2"
+        onSubmit={handleSubmit}
+      >
         <input
           type="search"
           placeholder="Search A Movie.."
@@ -27,7 +44,7 @@ const Main = () => {
         />
         <button type="submit">Search</button>
       </form>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap justify-center">
         <Cards movies={movies} />
       </div>
     </main>
