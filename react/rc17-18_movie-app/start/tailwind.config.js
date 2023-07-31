2bae63f05c15f9eede546cb1b6a6c6a1bd6569ca
KamilArici <kamilarici5@gmail.com>
2023-07-31 20:32:32 +0300

    dark mode

--- react/rc17-18_movie-app/start/tailwind.config.js
+++ react/rc17-18_movie-app/start/tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   plugins: [],
+  darkMode: "class",
 };
 
 //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
