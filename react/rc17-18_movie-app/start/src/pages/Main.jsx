9528a588c759c62db39daf94b285046a0d3dc24a
KamilArici <kamilarici5@gmail.com>
2023-08-01 21:53:03 +0300

    MovieContext

--- react/rc17-18_movie-app/start/src/pages/Main.jsx
+++ react/rc17-18_movie-app/start/src/pages/Main.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
 const Main = () => {
+  const { movies } = useContext(MovieContext);
+  console.log(movies);
   return <div>Main</div>;
 };
 
