cb1c6c94fa75602c0f846a31dfe9256327ed4635
KamilArici <kamilarici5@gmail.com>
2023-08-03 21:03:29 +0300

    muı

---
+++ react/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const Typo = () => {
+  return (
+    <div>
+      <Typography></Typography>
+    </div>
+  );
+};
+
+export default Typo;
