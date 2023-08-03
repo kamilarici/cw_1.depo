6c88f224ba8aa056b3da1179c8294b36a2988a2a
KamilArici <kamilarici5@gmail.com>
2023-08-03 21:35:15 +0300

    Typography

--- react/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
+++ react/rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -7,7 +7,23 @@ import Button from "@mui/material/Button";
 const Typo = () => {
   return (
     <div>
-      <Typography>MUI Typo</Typography>
+      <Typography variant="h5" component="h1" color="lightcoral" mt={4}>
+        MUI Typo
+      </Typography>
+      <Typography variant="button" color="lightcoral" mt={4}>
+        MUI Typo
+      </Typography>
+      <Typography variant="button" color="lightcoral" mt={4}>
+        MUI Typo
+      </Typography>
+      <Typography
+        sx={{ color: "blue", backgroundColor: "red" }}
+        variant="button"
+        color="lightcoral"
+        mt={4}
+      >
+        MUI Typo
+      </Typography>
     </div>
   );
 };
