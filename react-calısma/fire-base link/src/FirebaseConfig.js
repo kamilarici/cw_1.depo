6a6ce43e48d98c593f3bc522b72f5a4d11be87d3
KamilArici <kamilarici5@gmail.com>
2023-08-04 09:16:02 +0300

    firebaseConfig

--- react-calısma/fire-base link/src/FirebaseConfig.js
+++ react-calısma/fire-base link/src/FirebaseConfig.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-xhniL4PyI-Os73DVGxcoYS6sBnivLeg",
-    authDomain: "movie-app-74517.firebaseapp.com",
-    projectId: "movie-app-74517",
-    storageBucket: "movie-app-74517.appspot.com",
-    messagingSenderId: "329572943767",
-    appId: "1:329572943767:web:7aab86a5308322b8f7a6d2"
+  apiKey: "AIzaSyDxU-XsLssshQTbQFrBseB7KaEiDmhVhuk",
+  authDomain: "movie-app-5284b.firebaseapp.com",
+  projectId: "movie-app-5284b",
+  storageBucket: "movie-app-5284b.appspot.com",
+  messagingSenderId: "233849661111",
+  appId: "1:233849661111:web:26993405f83f5c8fdc4f32",
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
 export const auth = getAuth(app);
