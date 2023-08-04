7bbcaabb092d31e491f350475bd7bd264a9d948e
KamilArici <kamilarici5@gmail.com>
2023-08-04 23:48:18 +0300

    edit

--- react-tw/yeni-info/src/App.jsx
+++ react-tw/yeni-info/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <div className="d-flex justify-content-center align-items-center vh-100 mt-5">
       <Home />
     </div>
   );
