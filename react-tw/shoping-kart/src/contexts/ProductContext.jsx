45f3b8947851500e4119eb5a3c99386c88be3a2a
KamilArici <kamilarici5@gmail.com>
2023-08-05 23:33:01 +0300

    edit

--- react-tw/shoping-kart/src/contexts/ProductContext.jsx
+++ react-tw/shoping-kart/src/contexts/ProductContext.jsx
@@ -1,53 +1,24 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-const ProductContext = createContext();
+import React, { createContext, useState, useEffect } from "react";
+
+export const ProductContext = createContext();
+
 const ProductProvider = ({ children }) => {
-  const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
-  const [mokeData, setMokeData] = useState([]);
-  const fetchProducts = async () => {
-    try {
+  useEffect(() => {
+    const fetchProducts = async () => {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-
+      // console.log(data);
       setProducts(data);
-      setMokeData(data);
-      const category = await fetch(
-        "https://fakestoreapi.com/products/categories"
-      );
-      const categoryData = await category.json();
-
-      setCategories(categoryData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const filterProducts = (item) => {
-    console.log(item);
-    setProducts(mokeData.filter((product) => product.category === item));
-  };
-
-  const filterAllProducts = () => {
-    setProducts(mokeData);
-  };
-  useEffect(() => {
+    };
     fetchProducts();
   }, []);
 
-  const values = {
-    categories,
-    products,
-    filterProducts,
-    filterAllProducts,
-  };
-
   return (
-    <ProductContext.Provider value={values}>{children}</ProductContext.Provider>
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
   );
 };
 
 export default ProductProvider;
-
-export const useProductContext = () => {
-  return useContext(ProductContext);
-};
