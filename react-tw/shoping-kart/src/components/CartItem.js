f07535b9d33298a28488ea3e6423b6967927930f
KamilArici <kamilarici5@gmail.com>
2023-08-05 23:29:00 +0300

    edit

--- react-tw/shoping-kart/src/components/CartItem.js
+++ react-tw/shoping-kart/src/components/CartItem.js
@@ -3,50 +3,53 @@ import { Link } from "react-router-dom";
 import { IoMdClose, IoMdRemove, IoMdAdd } from "react-icons/io";
 
 const CartItem = ({ item }) => {
-
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b  border-gray-200 w-full font-light text-gray-500 ">
-    <div
-      className="w-full  min-h-[150px] flex
+      <div
+        className="w-full  min-h-[150px] flex
        items-center gap-x-4 px-4"
-    >
-      {/* item image */}
-      <Link to={`/product/4334`}>
-        <img className="max-w-[80px]" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
-      </Link>
-      {/* item title & remove icon */}
-      <div className="w-full flex flex-col">
-        <div className="flex justify-between mb-2">
-          <Link
-            to={`/product/4334`}
-            className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
-          >
-FJALLRAVEN - FOLDSACK NO. 1 BACKPACK, FITS 15 LAPTOPS
-          </Link>
+      >
+        <Link to={`/product/4334`}>
+          <img
+            className="max-w-[80px]"
+            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            alt=""
+          />
+        </Link>
+        {/* item title & remove icon */}
+        <div className="w-full flex flex-col">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/4334`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              FJALLRAVEN - FOLDSACK NO. 1 BACKPACK, FITS 15 LAPTOPS
+            </Link>
 
-          <div className="text-xl cursor-pointer" >
-            <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
-          </div>
-        </div>
-        <div className=" flex gap-x-2 h-[36px]">
-
-          <div className="flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium">
-            <div className="flex-1  h-full flex justify-center items-center cursor-pointer " >
-              <IoMdRemove />
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
-            <div className="h-full flex justify-center items-center px-2">
-           1
+          </div>
+          <div className=" flex gap-x-2 h-[36px]">
+            <div className="flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium">
+              <div className="flex-1  h-full flex justify-center items-center cursor-pointer ">
+                <IoMdRemove />
               </div>
-            <div className="flex-1 h-full flex justify-center items-center cursor-pointer " >
-              <IoMdAdd />
+              <div className="h-full flex justify-center items-center px-2">
+                1
+              </div>
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer ">
+                <IoMdAdd />
+              </div>
+            </div>
+
+            <div className="flex-1 flex justify-around items-center">$ 109</div>
+            <div className="flex-1 flex justify-end items-center text-primary font-medium">
+              109
             </div>
           </div>
-
-          <div className="flex-1 flex justify-around items-center">$ 109</div>
-          <div className="flex-1 flex justify-end items-center text-primary font-medium">109</div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
