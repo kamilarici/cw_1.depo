54d659a1a23aab9787e5ca431ffacd11496336e6
KamilArici <kamilarici5@gmail.com>
2023-08-05 23:30:33 +0300

    edit

--- react-tw/shoping-kart/src/components/CartItem.js
+++ react-tw/shoping-kart/src/components/CartItem.js
@@ -1,52 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { IoMdClose, IoMdRemove, IoMdAdd } from "react-icons/io";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
+import { CardContext } from "../contexts/CartContext";
 
-const CartItem = ({ item }) => {
+const CardItem = ({ item }) => {
+  const { removeFromCard, increaseAmount, decreaseAmount } =
+    useContext(CardContext);
+  const { id, title, image, price, amount } = item;
+
   return (
-    <div className="flex gap-x-4 py-2 lg:px-6 border-b  border-gray-200 w-full font-light text-gray-500 ">
-      <div
-        className="w-full  min-h-[150px] flex
-       items-center gap-x-4 px-4"
-      >
-        <Link to={`/product/4334`}>
-          <img
-            className="max-w-[80px]"
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-            alt=""
-          />
+    <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        {/* image */}
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px]" src={image} alt="" />
         </Link>
-        {/* item title & remove icon */}
-        <div className="w-full flex flex-col">
+        <div className="w-full flex-col ">
           <div className="flex justify-between mb-2">
             <Link
-              to={`/product/4334`}
+              to={`/product/${id}`}
               className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
             >
-              FJALLRAVEN - FOLDSACK NO. 1 BACKPACK, FITS 15 LAPTOPS
+              {title}
             </Link>
-
-            <div className="text-xl cursor-pointer">
+            <div
+              onClick={() => removeFromCard(id)}
+              className="text-xl cursor-pointer"
+            >
               <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
           </div>
-          <div className=" flex gap-x-2 h-[36px]">
-            <div className="flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium">
-              <div className="flex-1  h-full flex justify-center items-center cursor-pointer ">
+          <div className=" flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium  ">
+              <div
+                onClick={() => decreaseAmount(id)}
+                className="flex-1 h-full flex justify-center items-center cursor-pointer  "
+              >
                 <IoMdRemove />
               </div>
               <div className="h-full flex justify-center items-center px-2">
-                1
+                {amount}
               </div>
-              <div className="flex-1 h-full flex justify-center items-center cursor-pointer ">
+              <div
+                onClick={() => increaseAmount(id)}
+                className="flex-1 h-full flex justify-center items-center cursor-pointer"
+              >
                 <IoMdAdd />
               </div>
             </div>
-
-            <div className="flex-1 flex justify-around items-center">$ 109</div>
-            <div className="flex-1 flex justify-end items-center text-primary font-medium">
-              109
+            <div className="flex-1 flex items-center justify-around">
+              $ {price}
             </div>
+            <div className="flex-1 flex justify-end items-center text-primary font-medium">{`$ ${parseFloat(
+              price * amount
+            ).toFixed(2)}`}</div>
           </div>
         </div>
       </div>
@@ -54,4 +61,4 @@ const CartItem = ({ item }) => {
   );
 };
 
-export default CartItem;
+export default CardItem;
