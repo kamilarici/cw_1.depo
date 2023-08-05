13b1c4466bd4390ae97d21a46844e336807098d0
KamilArici <kamilarici5@gmail.com>
2023-08-05 23:32:35 +0300

    edit

--- react-tw/shoping-kart/src/components/Sidebar.js
+++ react-tw/shoping-kart/src/components/Sidebar.js
@@ -1,55 +1,57 @@
-import React from "react";
-import { useSidebarContext } from "../contexts/SidebarContext";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
 import { FiTrash2 } from "react-icons/fi";
 
-import CartItem from "./CartItem";
-import { Link } from "react-router-dom";
-
+import { SidebarContext } from "../contexts/SidebarContext";
+import CardItem from "./CarrdItem";
+import { CardContext } from "../contexts/CartContext";
 const Sidebar = () => {
-  const { handleClose, isOpen } = useSidebarContext();
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  const { card, clearCard, total, itemAmount } = useContext(CardContext);
 
   return (
     <div
       className={`${
-        isOpen ? "right-0 " : "-right-full"
-      } w-full md:w-[35vw] xl:max-w-[30vw] bg-white fixed top-0 h-full shadow-2xl  transition-all duration-300 z-20 lg:px[35px] `}
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white  fixed  top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px] `}
     >
-      {/* sidebar header */}
-      <div className="flex items-center justify-between py-6 px-3 border-b">
-        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold">
+          shopping bag({itemAmount})
+        </div>
         <div
-          className="cursor-pointer w-8 h-8 flex justify-center items-center"
           onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
         >
-          <IoMdArrowForward className="text-2xl" />
+          <IoMdArrowForward />
         </div>
       </div>
-      {/* sibebar cart items */}
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
-        <CartItem />;
+      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b ">
+        {card.map((item) => {
+          return <CardItem item={item} key={item.id} />;
+        })}
       </div>
-
-      <div className=" flex flex-col gap-y-3 py-4 mt-4 px-6">
-        <div className="flex w-full justify-between items-center">
+      <div className=" flex flex-col gap-y-3 py-4 mt-4">
+        <div className=" flex w-full justify-between items-center">
           <div className="uppercase font-semibold">
-            <span className="mr-2">Total:</span>$ 102
+            <span>Total:</span>$ {parseFloat(total).toFixed(2)}
           </div>
-          <div className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl">
+          <div
+            onClick={clearCard}
+            className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl "
+          >
             <FiTrash2 />
           </div>
         </div>
         <Link
-          to={"/"}
+          to="/"
           className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium"
         >
-          View Cart
+          View card{" "}
         </Link>
-        <Link
-          to={"/"}
-          className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium"
-        >
-          Checkout
+        <Link className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium">
+          Checkout{" "}
         </Link>
       </div>
     </div>
