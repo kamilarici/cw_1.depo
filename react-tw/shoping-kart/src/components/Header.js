b7c4f14d4092102c247143ace3a43e86d5924206
KamilArici <kamilarici5@gmail.com>
2023-08-05 23:32:05 +0300

    edit

--- react-tw/shoping-kart/src/components/Header.js
+++ react-tw/shoping-kart/src/components/Header.js
@@ -1,40 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useSidebarContext } from "../contexts/SidebarContext";
+import React, { useContext, useEffect, useState } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
 
 import { BsBag } from "react-icons/bs";
-import { FaBuysellads } from "react-icons/fa";
-
-
+import { CardContext } from "../contexts/CartContext";
 import { Link } from "react-router-dom";
+import Logo from "../img/logo.svg";
 
 const Header = () => {
-  const [isActive, setIsActive] = useState(false);
-  const { setIsOpen, isOpen } = useSidebarContext();
+  const [isActive, setIsActive] = useState(true);
 
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  const { itemAmount } = useContext(CardContext);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
     });
-  }, []);
+  });
 
   return (
     <header
-      className={` ${
+      className={`${
         isActive ? "bg-white py-4 shadow-md" : "bg-none py-6"
-      } fixed w-full z-10 transition-all`}
+      } fixed w-full z-10 transition-all `}
     >
-      <div className="container mx-auto flex justify-between h-full items-center">
+      <div className="container mx-auto flex items-center justify-between h-full ">
         <Link to={"/"}>
-          <FaBuysellads className="text-5xl" />
+          <img className="w-[40px]" src={Logo} alt="" />
         </Link>
+
         <div
-          className="cursor-pointer flex relative "
           onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer flex relative  "
         >
-          <BsBag className="text-2xl" />
-          <div className="bg-red-500 absolute -right-2 top-4 text-[12px] h-[15px] w-[15px] rounded-full flex justify-center items-center text-white p-2">
-            1
+          <BsBag />
+          <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items-center">
+            {itemAmount}
           </div>
         </div>
       </div>
