89d649747b32a2ade8c634eee1dbfdd4ada007eb
KamilArici <kamilarici5@gmail.com>
2023-08-05 19:06:11 +0300

    all

--- react-tw/shoping-kart/src/index.js
+++ react-tw/shoping-kart/src/index.js
@@ -4,15 +4,13 @@ import "./index.css";
 import App from "./App";
 
 import SidebarProvider from "./contexts/SidebarContext";
-
+import ProductProvider from "./contexts/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SidebarProvider>
-    
-      
-        <App />
-      
-    
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </SidebarProvider>
 );
--- react-tw/shoping-kart/src/pages/Home.js
+++ react-tw/shoping-kart/src/pages/Home.js
@@ -1,49 +1,40 @@
 import React from "react";
 import Product from "../components/Product";
 import Hero from "../components/Hero";
+import { useProductContext } from "../contexts/ProductContext";
 
 const Home = () => {
+  const { categories, products, filterProducts, filterAllProducts } =
+    useProductContext();
   return (
     <div>
       <Hero />
 
       <div className="container mx-auto mt-6 flex justify-center items-center gap-4 flex-wrap">
         <button
+          onClick={() => filterAllProducts()}
           type="button"
           className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
         >
           All
         </button>
-        <button
-          type="button"
-          className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
-        >
-          Electronics
-        </button>
-        <button
-          type="button"
-          className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
-        >
-          Jewelery
-        </button>
-        <button
-          type="button"
-          className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
-        >
-          Mens Clothing
-        </button>
-
-        <button
-          type="button"
-          className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
-        >
-          womnes Clothing
-        </button>
+        {categories.map((item, i) => (
+          <button
+            key={i}
+            type="button"
+            onClick={() => filterProducts(item)}
+            className="inline-block rounded-full border-4 border-[#f5e6e0b9] px-6 pb-[6px] pt-2 text-sm font-medium uppercase text-primary transition duration-300 ease-in-out  hover:bg-[#f5e6e0b9] "
+          >
+            {item}
+          </button>
+        ))}
       </div>
       <section className="py-10 ">
         <div className="container mx-auto">
           <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 max-w-sm mx-auto md:max-w-none md:mx-0">
-            <Product />
+            {products.map((item) => (
+              <Product key={item.id} item={item} />
+            ))}
           </div>
         </div>
       </section>
