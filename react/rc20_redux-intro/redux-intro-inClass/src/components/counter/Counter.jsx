219a5220c1bd96d9bba75a5844c83e0b77a01bb0
KamilArici <kamilarici5@gmail.com>
2023-08-05 12:56:24 +0300

    redux

--- react/rc20_redux-intro/redux-intro-inClass/src/components/counter/Counter.jsx
+++ react/rc20_redux-intro/redux-intro-inClass/src/components/counter/Counter.jsx
@@ -1,17 +1,38 @@
-import "./Counter.css"
+import { useSelector, useDispatch } from "react-redux";
+import "./Counter.css";
 
 const Counter = () => {
+  //? Global state'in tüketilmesi
+  const count = useSelector((state) => state.count);
+
+  //? useDispatch custom redux hookudur
+  const dispatch = useDispatch();
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
-      <h1>counter:0</h1>
+      <h1>counter:{count}</h1>
       <div>
-        <button className="counter-button positive">increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button
+          className="counter-button positive"
+          onClick={() => dispatch({ type: "INC" })}
+        >
+          increase
+        </button>
+        <button
+          className="counter-button zero"
+          onClick={() => dispatch({ type: "CLR" })}
+        >
+          reset
+        </button>
+        <button
+          className="counter-button negative"
+          onClick={() => dispatch({ type: "DEC" })}
+        >
+          decrease
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
---
+++ react/rc20_redux-intro/redux-intro-inClass/src/store/counterReducer.js
@@ -0,0 +1,21 @@
+//? State'lerin baslangic durumlarinin belirlenmesi
+const initialState = {
+  count: 10,
+};
+
+//? Pure Function (reducer)
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC": //? count:  state.count + 1
+      return { count: state.count + 1 };
+
+    case "DEC":
+      return { count: state.count - 1 };
+
+    case "CLR":
+      return { count: 0 };
+    default:
+      return state;
+    //! Reducer fonksiyonu muhakkak bir state objesi dondurmelidir. Yanlis bir type gelse bile state'in ilk halini dondurmelidir.
+  }
+};
