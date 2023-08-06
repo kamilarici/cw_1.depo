2ce71883db13c80f2a649bca6f7ddd4153d036fb
KamilArici <kamilarici5@gmail.com>
2023-08-06 16:15:22 +0300

    redux start

---
+++ react/rc20_redux-intro/redux-intro-starter/src/store/counterReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  count: 0,
+};
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC":
+      return { count: state.count + 1 };
+    case "DEC":
+      return { count: state.count - 1 };
+    case "CLR":
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
---
+++ react/rc20_redux-intro/redux-intro-starter/src/store/index.jsx
@@ -0,0 +1,3 @@
+import { createStore } from "redux";
+import { counterReducer } from "./counterReducer";
+export const store = createStore(counterReducer);
