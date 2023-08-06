16f8fb51ea6854308d28cad25d831ac40c513006
KamilArici <kamilarici5@gmail.com>
2023-08-06 17:23:53 +0300

    edit

--- react/rc20_redux-intro/redux-intro-starter/src/store/counterReducer.js
+++ react/rc20_redux-intro/redux-intro-starter/src/store/counterReducer.js
@@ -2,6 +2,15 @@ export const INC = "INC";
 export const DEC = "DEC";
 export const CLR = "CLR";
 
+// export const  increment=(payload)=>{
+//     return {type:INC,payload}
+// }
+export const increment = () => {
+  return { type: INC };
+};
+export const decrement = () => ({ type: DEC });
+export const clear = () => ({ type: CLR });
+
 const initialState = {
   count: 10,
 };
