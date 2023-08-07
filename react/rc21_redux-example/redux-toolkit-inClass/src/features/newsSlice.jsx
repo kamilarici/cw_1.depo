e9d629235caa6a0db5b487585a028464f8fad8c8
KamilArici <kamilarici5@gmail.com>
2023-08-07 22:34:01 +0300

    edit

--- react/rc21_redux-example/redux-toolkit-inClass/src/features/newsSlice.jsx
+++ react/rc21_redux-example/redux-toolkit-inClass/src/features/newsSlice.jsx
@@ -1,10 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   news: [],
 };
 
-const getNews = () => {};
+const getNews = createAsyncThunk(
+  "getNewsFunc", //? action type name
+
+  async () => {
+    const API_KEY = "47d350b7a633401694e0fa5b4ddcfd46";
+    const url = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
+    const { data } = await axios(url);
+    console.log(data);
+  }
+);
 
 const newsSlice = createSlice({
   name: "news",
