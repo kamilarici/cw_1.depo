5bd83f4a2482204611609e2746881c7a2c9d7c09
KamilArici <kamilarici5@gmail.com>
2023-08-07 21:05:16 +0300

    authSlice.actions

--- react/rc21_redux-example/redux-toolkit-inClass/src/features/authSlice.jsx
+++ react/rc21_redux-example/redux-toolkit-inClass/src/features/authSlice.jsx
@@ -4,18 +4,24 @@ const initialState = {
   user: "",
 };
 
+//? Slice ile bir global state'in hem action type'lari , hem action creator fonksiyonlarini hem de reducer'i tek bir hamlede olusturabiliriz.
+
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
+  name: "auth", //? action type'ı olusturmak icin
+  initialState, //? state'in bslangic degeri icin
   reducers: {
+    //? action creator fonks. ve reducer icin
     setUser: (state, action) => {
       state.user = action.payload;
     },
-    clearUser: (state, action) => {
+    clearUser: (state) => {
       state.user = "";
     },
   },
 });
 
+//? olusan action fonksiyonları sliceAdi.actions 'dan destructure edilerek export edilir.
 export const { setUser, clearUser } = authSlice.actions;
+
+//? reducer ise sliceAdi.reducer seklinde export edilmelidir.
 export default authSlice.reducer;
