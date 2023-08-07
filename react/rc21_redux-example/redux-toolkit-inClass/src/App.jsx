2193cd6c0268ccb392c8cf03f8254e6e2591de76
KamilArici <kamilarici5@gmail.com>
2023-08-07 21:31:11 +0300

    authReducer

--- react/rc21_redux-example/redux-toolkit-inClass/src/App.jsx
+++ react/rc21_redux-example/redux-toolkit-inClass/src/App.jsx
@@ -1,11 +1,15 @@
-import AppRouter from "./router/AppRouter"
+import { store } from "./app/store";
+import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <div>
-      <AppRouter />
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
