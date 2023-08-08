224eceac667ce2c93931d79b4bbd1ef9f85f643b
KamilArici <kamilarici5@gmail.com>
2023-08-08 17:58:20 +0300

    toolkit

---
+++ react-calısma/redux-tollkit/src/features/counterSlice.js
@@ -0,0 +1,14 @@
+// rxslice
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const counterSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {},
+});
+
+export const {} = counterSlice.actions;
+
+export default counterSlice.reducer;
