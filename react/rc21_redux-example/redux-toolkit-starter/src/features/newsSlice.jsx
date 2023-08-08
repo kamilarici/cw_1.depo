5aeaa27dcdce56897d454a767bbc496146272016
KamilArici <kamilarici5@gmail.com>
2023-08-08 14:19:28 +0300

    axios

--- react/rc21_redux-example/redux-toolkit-starter/src/features/newsSlice.jsx
+++ react/rc21_redux-example/redux-toolkit-starter/src/features/newsSlice.jsx
@@ -1,8 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 const initialState = {
   news: [],
 };
+
+const getNews = createAsyncThunk("getNewsFunc", async () => {
+  const API_KEY = "47d350b7a633401694e0fa5b4ddcfd46";
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+});
 
 const newsSlice = createSlice({
   name: "news",
--- react/rc21_redux-example/redux-toolkit-starter/src/router/PrivateRouter.jsx
+++ react/rc21_redux-example/redux-toolkit-starter/src/router/PrivateRouter.jsx
@@ -5,7 +5,7 @@ const PrivateRouter = () => {
   // const user = true;
 
   const user = useSelector((state) => state.auth.user);
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  return user?.email ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRouter;
