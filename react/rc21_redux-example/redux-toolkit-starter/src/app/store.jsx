310fcce4a76da7bdc673797c7a8a125f8bde22e1
KamilArici <kamilarici5@gmail.com>
2023-08-08 10:17:21 +0300

    authSlice.reducer

--- react/rc21_redux-example/redux-toolkit-starter/src/app/store.jsx
+++ react/rc21_redux-example/redux-toolkit-starter/src/app/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    // auth: authReducer,
+    // api:newsReducer
   },
 });
--- react/rc21_redux-example/redux-toolkit-starter/src/features/authSlice.jsx
+++ react/rc21_redux-example/redux-toolkit-starter/src/features/authSlice.jsx
@@ -3,3 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: "",
 };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducer: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = "";
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
