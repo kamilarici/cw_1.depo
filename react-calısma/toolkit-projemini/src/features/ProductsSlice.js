46db6cf09c53c2f246a80a74fa390ee476122d98
KamilArici <kamilarici5@gmail.com>
2023-08-09 16:03:48 +0300

    edit

--- react-calısma/toolkit-projemini/src/features/ProductsSlice.js
+++ react-calısma/toolkit-projemini/src/features/ProductsSlice.js
@@ -3,11 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   products: [],
 };
+// export const getProducts = () => {
+//   return async function getProductThunk(dispatch){
+
+//     const data = await fetch("https://fakestoreapi.com/products");
+//     const result = await data.json();
+// dispatch(fetchProducts(result))
+//   }
+// };
 
 const ProductsSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  reducers: {
+    //     fetchProducts:(state,action)=>{
+    // state.products=action.payload
+    //     }
+  },
 });
 
 export const {} = ProductsSlice.actions;
--- react-calısma/toolkit-projemini/src/pages/Home.jsx
+++ react-calısma/toolkit-projemini/src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await fetch("https://fakestoreapi.com/products");
+      const result = await data.json();
+      console.log(result);
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div>
       <h1>PRODUCTS</h1>
