6d6d10a36685093cf2719610df70d42d81c4f6a2
KamilArici <kamilarici5@gmail.com>
2023-08-10 23:25:17 +0300

    stockSlice

---
+++ react/rc22-26_stock-app/stock2-app-inClass2/src/features/stockSlice.jsx
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stockSlice = createSlice({
+  name: "stock",
+
+  initialState: {
+    loading: false,
+    error: false,
+    sales: [],
+    purchases: [],
+    firms: [],
+    categories: [],
+    brands: [],
+    products: [],
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    getFirmsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.firms = payload;
+    },
+    getBrandsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.brands = payload;
+    },
+
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { fetchStart, fetchFail, getFirmsSuccess } = stockSlice.actions;
+export default stockSlice.reducer;
