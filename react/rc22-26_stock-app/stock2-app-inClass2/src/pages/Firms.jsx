d1c8036b761230f2e15e92d5b61c092037fa2e7c
KamilArici <kamilarici5@gmail.com>
2023-08-10 23:29:08 +0300

    edit

--- react/rc22-26_stock-app/stock2-app-inClass2/src/pages/Firms.jsx
+++ react/rc22-26_stock-app/stock2-app-inClass2/src/pages/Firms.jsx
@@ -1,5 +1,51 @@
-const Firms = () => {
-  return <div>Firms</div>
-}
+import { useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-export default Firms
+const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+
+  const getFirms = async () => {
+    try {
+      const { data } = await axios(
+        `${import.meta.env.VITE_BASE_URL}/stock/firms/`,
+        {
+          headers: { Authorization: `Token ${token}` },
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getSales = async () => {
+    try {
+      const { data } = await axios(
+        `${import.meta.env.VITE_BASE_URL}/stock/sales/`,
+        {
+          headers: { Authorization: `Token ${token}` },
+        }
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color={"error"} mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">NEW FIRM</Button>
+    </div>
+  );
+};
+
+export default Firms;
