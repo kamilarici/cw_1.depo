15438fdc323b09f8e78f878dee8adbf8e1413996
KamilArici <kamilarici5@gmail.com>
2023-08-10 23:18:42 +0300

    edit

--- react/rc22-26_stock-app/stock2-app-inClass2/src/pages/Dashboard.jsx
+++ react/rc22-26_stock-app/stock2-app-inClass2/src/pages/Dashboard.jsx
@@ -1,81 +1,124 @@
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import InventoryIcon from "@mui/icons-material/Inventory";
-import StoreIcon from "@mui/icons-material/Store";
-import StarsIcon from "@mui/icons-material/Stars";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
 
-const icons = [
-  {
-    icon: <DashboardCustomizeIcon />,
-    title: "Dashboard",
-    url: "/stock/",
-  },
-  {
-    title: "Purchase",
-    icon: <ShoppingCartIcon />,
-    url: "/stock/purchases/",
-  },
-  {
-    title: "Sales",
-    icon: <AttachMoneyIcon />,
-    url: "/stock/sales/",
-  },
-  {
-    title: "Firms",
-    icon: <StoreIcon />,
-    url: "/stock/firms/",
-  },
-  {
-    title: "Brands",
-    icon: <StarsIcon />,
-    url: "/stock/brands/",
-  },
-  {
-    title: "Products",
-    icon: <InventoryIcon />,
-    url: "/stock/products/",
-  },
-  {
-    title: "Admin Panel",
-    icon: <SupervisorAccountIcon />,
-    url: "https://10001.fullstack.clarusway.com/admin",
-  },
-];
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const MenuListItems = () => {
-  const navigate = useNavigate();
-  return (
+import MenuListItems from "../components/MenuListItems";
+import { Outlet } from "react-router-dom";
+import useAuthCall from "../hooks/useAuthCall";
+
+const drawerWidth = 200;
+
+function Dashboard(props) {
+  const { logout } = useAuthCall();
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
     <div>
-      <List>
-        {icons.map((item, index) => (
-          <ListItem
-            key={index}
-            disablePadding
-            onClick={() => navigate(item.url)}
-            sx={{
-              color: "white",
-              "& .MuiSvgIcon-root": { color: "white" },
-              "&:hover": { color: "red" },
-              "&:hover .MuiSvgIcon-root": { color: "red" },
-            }}
-          >
-            <ListItemButton>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <Toolbar />
+      <Divider />
+      <MenuListItems />
     </div>
   );
-};
 
-export default MenuListItems;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Stock App
+          </Typography>
+
+          <Button variant="contained" onClick={() => logout()}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "secondary.main",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "secondary.main",
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+}
+
+export default Dashboard;
