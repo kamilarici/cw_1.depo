e497c164ff7650fb5be4a4597056c5937cf057b8
KamilArici <kamilarici5@gmail.com>
2023-08-11 16:45:48 +0300

    stockslice

--- react/rc22-26_stock-app/stock2-app-starter2/src/pages/Firms.jsx
+++ react/rc22-26_stock-app/stock2-app-starter2/src/pages/Firms.jsx
@@ -1,5 +1,35 @@
-const Firms = () => {
-  return <div>Firms</div>
-}
+import { Button, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-export default Firms
+const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const getFirms = async () => {
+    const BASE_URL = "https://14108.fullstack.clarusway.com";
+
+    try {
+      const { data } = await axios(`${BASE_URL}/stock/firms/`, {
+        headers: { Authorization: `Token ${token} ` },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color={"error"} mb={4}>
+        Firms
+      </Typography>
+      <Button variant="contained">NEW FİRM</Button>
+    </div>
+  );
+};
+
+export default Firms;
