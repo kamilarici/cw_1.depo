1d712c7a446a0015b133cf171c52c5406bcc9613
KamilArici <kamilarici5@gmail.com>
2023-08-11 17:00:50 +0300

    stockslice

--- react/rc22-26_stock-app/stock2-app-starter2/src/pages/Firms.jsx
+++ react/rc22-26_stock-app/stock2-app-starter2/src/pages/Firms.jsx
@@ -2,18 +2,24 @@ import { Button, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-
+import { fetchFail, fetchStart } from "../features/authSlice";
+import { useDispatch } from "react-redux";
+import { getFirmsSuccess } from "../features/stockSlice";
 const Firms = () => {
   const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const getFirms = async () => {
+    dispatch(fetchStart());
     const BASE_URL = "https://14108.fullstack.clarusway.com";
 
     try {
       const { data } = await axios(`${BASE_URL}/stock/firms/`, {
         headers: { Authorization: `Token ${token} ` },
       });
+      dispatch(getFirmsSuccess(data));
       console.log(data);
     } catch (error) {
+      dispatch(fetchFail());
       console.log(error);
     }
   };
