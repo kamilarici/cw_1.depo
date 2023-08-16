ee7fdcf69b52ab1c23690e95a1e22733343f206f
KamilArici <kamilarici5@gmail.com>
2023-08-16 20:39:54 +0300

    produc

--- react/rc22-26_stock-app/stock-app-inClass2-5/src/pages/Products.jsx
+++ react/rc22-26_stock-app/stock-app-inClass2-5/src/pages/Products.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+
 import { useSelector } from "react-redux";
 import useStockCall from "../hooks/useStockCall";
-import FirmCard from "../components/FirmCard";
-import FirmModal from "../components/FirmModal";
-import ProductTable from "../components/ProductTable";
+
+import ProductModal from "../components/ProductModal";
+import ProductTable from "../components/Producttable";
 
 const Products = () => {
   const { getStockData } = useStockCall();
-  const { firms } = useSelector((state) => state.stock);
+  const { products } = useSelector((state) => state.stock);
 
   const [info, setInfo] = useState({
     name: "",
@@ -27,12 +27,24 @@ const Products = () => {
   };
 
   useEffect(() => {
-    // getFirms()
-    getStockData("firms");
+    getStockData("products");
   }, []);
 
   return (
     <div>
+      <Typography variant="h4" color={"error"} mb={3}>
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        NEW PRODUCT
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
       <ProductTable />
     </div>
   );
