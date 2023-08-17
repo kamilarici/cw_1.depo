71da43985931e4f7d0d40bda574ea7cc52683382
KamilArici <kamilarici5@gmail.com>
2023-08-17 22:25:39 +0300

    edit

--- react/rc22-26_stock-app/stock-app-inClass6/src/components/Charts.jsx
+++ react/rc22-26_stock-app/stock-app-inClass6/src/components/Charts.jsx
@@ -2,7 +2,7 @@ import { Card, Title, LineChart } from "@tremor/react";
 import { useSelector } from "react-redux";
 
 const dataFormatter = (number) =>
-  `${Intl.NumberFormat("us").format(number).toString()}%`;
+  `${Intl.NumberFormat("us").format(number).toString()}`;
 
 const Charts = () => {
   const { sales } = useSelector((state) => state.stock);
@@ -10,20 +10,20 @@ const Charts = () => {
   const salesData = sales?.map((item) => ({
     date: item.createds,
     quantity: item.quantity,
-    price: item.price_total,
+    price: Number(item.price_total),
   }));
-  console.log(sales);
+
+  console.log(salesData);
   return (
     <Card>
       <Title>Total Sales</Title>
       <LineChart
         className="mt-4"
         data={salesData}
-        index="year"
-        categories={["Export Growth Rate", "Import Growth Rate"]}
-        colors={["emerald", "gray"]}
+        index="date"
+        categories={["quantity", "price"]}
+        colors={["red", "blue"]}
         valueFormatter={dataFormatter}
-        yAxisWidth={40}
       />
     </Card>
   );
