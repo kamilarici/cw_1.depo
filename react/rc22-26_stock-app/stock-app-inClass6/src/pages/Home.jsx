881860bfd625d3f323e2cdf50515c9718c585f6f
KamilArici <kamilarici5@gmail.com>
2023-08-17 21:05:48 +0300

    edit

--- react/rc22-26_stock-app/stock-app-inClass6/src/pages/Home.jsx
+++ react/rc22-26_stock-app/stock-app-inClass6/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { Typography } from "@mui/material";
+import KpiCards from "../components/KpiCards";
+import Charts from "../components/Charts";
 const Home = () => {
-  return <div>Home</div>
-}
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={2}>
+        Dashboard
+      </Typography>
 
-export default Home
+      <KpiCards />
+
+      <Charts />
+    </div>
+  );
+};
+
+export default Home;
