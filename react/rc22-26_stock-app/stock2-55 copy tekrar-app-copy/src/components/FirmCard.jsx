28a129654676cc737e760d37b671c735005cdb00
KamilArici <kamilarici5@gmail.com>
2023-08-17 15:04:29 +0300

    start

---
+++ react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/FirmCard.jsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
+import EditIcon from "@mui/icons-material/Edit"
+import { btnStyle } from "../styles/globalStyles"
+
+export default function FirmCard({ firm }) {
+  return (
+    <Card
+      sx={{
+        p: 2,
+        width: "300px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {firm.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {firm.address}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        sx={{ p: 1, objectFit: "contain", height: 140 }}
+        image={firm.image}
+        title={firm.name}
+        component="img"
+      />
+
+      <Typography variant="body2" color="text.secondary">
+        {firm.phone}
+      </Typography>
+
+      <CardActions>
+        <EditIcon sx={btnStyle} />
+        <DeleteOutlineIcon sx={btnStyle} />
+      </CardActions>
+    </Card>
+  )
+}
