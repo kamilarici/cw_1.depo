ecb3d5ce459b4b1d9674dc06ab512217b7a03339
KamilArici <kamilarici5@gmail.com>
2023-08-21 14:09:33 +0300

    paper

--- react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/KpiCards.jsx
+++ react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/KpiCards.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { deepPurple, pink, amber } from "@mui/material/colors";
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
+import PaymentsIcon from "@mui/icons-material/Payments";
+import { Grid } from "@mui/material";
 const KpiCards = () => {
-  return <div>KpiCards</div>;
+  const cardData = [
+    {
+      id: 1,
+      icon: <MonetizationOnIcon />,
+      bgColor: deepPurple[200],
+      color: deepPurple[700],
+      title: "sales",
+      value: "$48000",
+    },
+    {
+      id: 1,
+      icon: <ShoppingCartIcon />,
+      bgColor: pink[200],
+      color: pink[700],
+      title: "profit",
+      value: "$32000",
+    },
+    {
+      id: 1,
+      icon: <PaymentsIcon />,
+      bgColor: amber[200],
+      color: amber[700],
+      title: "purchases",
+      value: "$4800",
+    },
+  ];
+  return (
+    <div>
+      <Grid container>
+        {cardData.map((item) => (
+          <Grid item></Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default KpiCards;
