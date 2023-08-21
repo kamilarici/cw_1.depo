83314af1c1fff779975e75464f92b16fe24e7d36
KamilArici <kamilarici5@gmail.com>
2023-08-21 16:34:43 +0300

    home

--- react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/Charts.jsx
+++ react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/Charts.jsx
@@ -1,50 +1,54 @@
 import { Card, Title, LineChart } from "@tremor/react";
-
-const chartdata = [
-  {
-    year: 1970,
-    "Export Growth Rate": 2.04,
-    "Import Growth Rate": 1.53,
-  },
-  {
-    year: 1971,
-    "Export Growth Rate": 1.96,
-    "Import Growth Rate": 1.58,
-  },
-  {
-    year: 1972,
-    "Export Growth Rate": 1.96,
-    "Import Growth Rate": 1.61,
-  },
-  {
-    year: 1973,
-    "Export Growth Rate": 1.93,
-    "Import Growth Rate": 1.61,
-  },
-  {
-    year: 1974,
-    "Export Growth Rate": 1.88,
-    "Import Growth Rate": 1.67,
-  },
-  //...
-];
+import { useSelector } from "react-redux";
+import Grid from "@mui/material/Grid";
 
 const dataFormatter = (number) =>
-  `${Intl.NumberFormat("us").format(number).toString()}%`;
+  `${Intl.NumberFormat("us").format(number).toString()}`;
 
-const Charts = () => (
-  <Card>
-    <Title>Export/Import Growth Rates (1970 to 2021)</Title>
-    <LineChart
-      className="mt-6"
-      data={chartdata}
-      index="year"
-      categories={["Export Growth Rate", "Import Growth Rate"]}
-      colors={["emerald", "gray"]}
-      valueFormatter={dataFormatter}
-      yAxisWidth={40}
-    />
-  </Card>
-);
+const Charts = () => {
+  const { sales, purchases } = useSelector((state) => state.stock);
 
+  const salesData = sales?.map((item) => ({
+    date: item.createds,
+    quantity: item.quantity,
+    price: Number(item.price_total),
+  }));
+
+  const purchasesData = purchases?.map((item) => ({
+    date: item.createds,
+    price: Number(item.price_total),
+  }));
+
+  return (
+    <Grid container justifyContent="center" spacing={3} mt={4}>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <Title>Total Sales</Title>
+          <LineChart
+            className="mt-4"
+            data={salesData}
+            index="date"
+            categories={["quantity", "price"]}
+            colors={["red", "blue"]}
+            valueFormatter={dataFormatter}
+          />
+        </Card>
+      </Grid>
+
+      <Grid item xs={12} md={6}>
+        <Card>
+          <Title>Total Purchases</Title>
+          <LineChart
+            className="mt-4"
+            data={purchasesData}
+            index="date"
+            categories={["price"]}
+            colors={["green"]}
+            valueFormatter={dataFormatter}
+          />
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
 export default Charts;
