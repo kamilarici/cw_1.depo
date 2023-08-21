b3f9b1d94010d0b0356c7b6954950a3f2b21feae
KamilArici <kamilarici5@gmail.com>
2023-08-21 10:41:27 +0300

    edit

--- react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/ProductTable.jsx
+++ react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/components/ProductTable.jsx
@@ -1,24 +1,32 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: "id", headerName: "#", headerAlign: "center", fex: 0.5 },
   {
-    field: "firstName",
+    field: "category",
     headerName: "First name",
     width: 150,
     editable: true,
   },
   {
-    field: "lastName",
-    headerName: "Last name",
+    field: "brand",
+    headerName: "brand",
     width: 150,
     editable: true,
   },
   {
-    field: "age",
-    headerName: "Age",
+    field: "name",
+    headerName: "name",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "stock",
+    headerName: "stock",
     type: "number",
     width: 110,
     editable: true,
@@ -34,23 +42,12 @@ const columns = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
 export default function ProductTable() {
+  const { products } = useSelector((state) => state.stock);
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={products}
         columns={columns}
         initialState={{
           pagination: {
