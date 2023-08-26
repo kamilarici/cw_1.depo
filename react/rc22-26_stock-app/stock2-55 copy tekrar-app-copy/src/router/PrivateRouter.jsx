c11f6899da0c68c9d8ef1f4c8986f27e4ec23d13
KamilArici <kamilarici5@gmail.com>
2023-08-26 18:32:29 +0300

    edit

--- react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/router/PrivateRouter.jsx
+++ react/rc22-26_stock-app/stock2-55 copy tekrar-app-copy/src/router/PrivateRouter.jsx
@@ -1,11 +1,12 @@
-import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-  const { currentUser } = useSelector((state) => state.auth)
+  const { currentUser } = useSelector((state) => state.auth);
 
   // const currentUser = true
-  return currentUser ? <Outlet /> : <Navigate to="/" />
-}
 
-export default PrivateRouter
+  return currentUser ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRouter;
