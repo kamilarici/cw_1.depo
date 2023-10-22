2df300bb7e47015fbecd7da7a18b0a31f13a5b5f
kamilarici <kamil@testinvite.com>
2023-10-22 19:54:48 +0300

    generic

--- typescript-2/ts2/out/App.js
+++ typescript-2/ts2/out/App.js
@@ -1,5 +1,5 @@
 const person1 = {
-    name: "kamil"
+    name: "kamil",
 };
 class LSMCalender {
     constructor(events) {
@@ -10,7 +10,25 @@ class LSMCalender {
         this.events.push(event);
     }
 }
-const cohort14 = new LSMCalender(['HTML', 'JS']);
-cohort14.addEvents('react');
+const cohort14 = new LSMCalender(["HTML", "JS"]);
+cohort14.addEvents("react");
 console.log(cohort14);
 const point1 = { x: 50, y: 30 };
+const point2 = { x: 50, y: 30 };
+function add(n1, n2) {
+    return n1 + n2;
+}
+function conC(n1, n2) {
+    return n1 + n2;
+}
+let arithmeticAdd = add;
+console.log(arithmeticAdd(2, 8)); //10 verir hata yok
+// arithmeticAdd=conC //hata verir çünkü type lar eşitleyemez
+//! GENERİCS
+// build in generic
+const names = [];
+const names2 = ["hello", "world"];
+const names3 = [1, 2, 3];
+const myData = { data: 35 };
+const myData2 = { data: '35' };
+const myData3 = { data: false };
--- typescript-2/ts2/src/App.ts
+++ typescript-2/ts2/src/App.ts
@@ -1,36 +1,74 @@
-interface Person{
-    name:string;
+interface Person {
+  name: string;
 }
 
-const person1:Person={
-    name:"kamil"
+const person1: Person = {
+  name: "kamil",
+};
+
+interface Calender {
+  events: string[];
+  addEvents(event: string): void;
+}
+class LSMCalender implements Calender {
+  constructor(public events: string[]) {
+    this.events;
+  }
+  addEvents(event: string): void {
+    this.events.push(event);
+  }
 }
 
-interface Calender{
-    events:string[];
-    addEvents(event:string):void;
+const cohort14 = new LSMCalender(["HTML", "JS"]);
 
-}
-class LSMCalender implements Calender{
-    constructor( public events:string[]){
-        this.events
-    }
-    addEvents(event: string): void {
-        this.events.push(event)
-    }
+cohort14.addEvents("react");
+console.log(cohort14);
+
+interface Point {
+  x: number;
 }
 
-const cohort14=new LSMCalender(['HTML','JS'])
+interface Point {
+  y: number;
+}
+const point1: Point = { x: 50, y: 30 };
 
-cohort14.addEvents('react')
-console.log(cohort14)
+type Pointx = { x: number };
+type Pointy = { y: number };
+type PointI = Pointx & Pointy;
 
+const point2: PointI = { x: 50, y: 30 };
 
-interface Point{
-    x:number;
+// fonksiyon imzası
+interface Sum {
+  (num1: number, num2: number): number;
+}
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+function conC(n1: string, n2: string): string {
+  return n1 + n2;
 }
 
-interface Point{
-    y:number
+let arithmeticAdd: Sum = add;
+console.log(arithmeticAdd(2, 8)); //10 verir hata yok
+
+// arithmeticAdd=conC //hata verir çünkü type lar eşitleyemez
+
+//! GENERİCS
+
+// build in generic
+
+const names: string[] = [];
+const names2: Array<string> = ["hello", "world"];
+const names3: Array<number> = [1, 2, 3];
+
+
+// problem
+interface Result<T>{
+    data:T | null
 }
-const point1:Point={x:50,y:30}
+
+const myData:Result<number>={data:35}
+const myData2:Result<string>={data:'35'}
+const myData3:Result<boolean>={data:false}
